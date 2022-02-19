da96b94e0221e21434468cd5ec89001a5d2cb23f
Neemias Santos <neemiassgc@outlook.com>
2022-02-19 09:43:24 -0300

    Initial files

---
+++ src/test/java/com/xyz/ean/EanApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xyz.ean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EanApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
