7edd2b4e52551486c793151d6115586ddcebbd0c
Neemias Santos <neemiassgc@outlook.com>
2022-03-01 07:51:14 -0300

    Add doubleParse method to the DomainUtils class

--- src/main/java/com/xyz/ean/pojo/DomainUtils.java
+++ src/main/java/com/xyz/ean/pojo/DomainUtils.java
@@ -1,4 +1,8 @@
 package com.xyz.ean.pojo;
 
 public final class DomainUtils {
+
+    public static double parsePrice(final String priceInput) {
+        return Double.parseDouble(priceInput.replace(",", "."));
+    }
 }
