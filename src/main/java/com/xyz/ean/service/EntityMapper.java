0efc59c4968141e51a2c073149e2f7d55c3e1261
Neemias Santos <neemiassgc@outlook.com>
2022-03-01 07:54:59 -0300

    Add mapProduct method to the EntityMapper class service

--- src/main/java/com/xyz/ean/service/EntityMapper.java
+++ src/main/java/com/xyz/ean/service/EntityMapper.java
@@ -1,8 +1,21 @@
 package com.xyz.ean.service;
 
+import com.xyz.ean.entity.Price;
+import com.xyz.ean.entity.Product;
+import com.xyz.ean.pojo.dto.DomainResponse;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EntityMapper {
 
+    public Product mapProduct(final DomainResponse domainResponse) {
+        final Product product = new Product();
+        final Price price = new Price();
+        product.setDescription(domainResponse.getDescription());
+        product.setSequenceCode(domainResponse.getSequence());
+        product.setEanCode(domainResponse.getEanCode());
+        price.setPrice(domainResponse.getPrice());
+        product.addPrice(price);
+        return product;
+    }
 }
