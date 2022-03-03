2ed3fa5bbed5fbff2eb6a1b3e35783adb847d669
Neemias Santos <neemiassgc@outlook.com>
2022-03-03 19:37:04 -0300

    Initialize the TargetProductIntegrationService class service with the needed beans

--- src/main/java/com/xyz/ean/service/EanService.java
+++
@@ -1,8 +0,0 @@
-package com.xyz.ean.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class EanService {
-
-}
--- src/main/java/com/xyz/ean/service/TargetProductIntegrationService.java
+++ src/main/java/com/xyz/ean/service/TargetProductIntegrationService.java
@@ -1,8 +1,45 @@
 package com.xyz.ean.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.xyz.ean.pojo.SessionInstance;
+import org.apache.http.impl.client.BasicCookieStore;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.DefaultRedirectStrategy;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.function.Supplier;
 
 @Service
 public class TargetProductIntegrationService {
 
+    private final RestTemplate restTemplate;
+    private final ObjectMapper objectMapper;
+    private final EntityMapper entityMapper;
+
+    private SessionInstance sessionInstance;
+
+    @Autowired
+    public TargetProductIntegrationService(final RestTemplateBuilder restTemplateBuilder, final ObjectMapper objectMapper, final EntityMapper entityMapper) {
+        final Supplier<RestTemplate> restTemplateSupplier = () -> {
+            final CloseableHttpClient httpClient =
+                HttpClientBuilder.create()
+                    .setRedirectStrategy(DefaultRedirectStrategy.INSTANCE)
+                    .setDefaultCookieStore(new BasicCookieStore())
+                    .build();
+
+            return restTemplateBuilder
+                .rootUri("https://apex.savegnago.com.br/apexmobile")
+                .requestFactory(() -> new HttpComponentsClientHttpRequestFactory(httpClient))
+                .build();
+        };
+
+        this.restTemplate = restTemplateSupplier.get();
+        this.objectMapper = objectMapper;
+        this.entityMapper = entityMapper;
+    }
 }
