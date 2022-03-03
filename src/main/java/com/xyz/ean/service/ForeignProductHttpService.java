648b06f579c76cd66b18abd9ea349a518224d707
Neemias Santos <neemiassgc@outlook.com>
2022-03-03 19:42:18 -0300

    Rename TargetProductIntegrationService service class to ForeignProductHttpService

--- src/main/java/com/xyz/ean/service/ForeignProductHttpService.java
+++ src/main/java/com/xyz/ean/service/ForeignProductHttpService.java
@@ -1,18 +1,34 @@
 package com.xyz.ean.service;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.xyz.ean.entity.Product;
+import com.xyz.ean.pojo.DomainUtils;
 import com.xyz.ean.pojo.SessionInstance;
+import com.xyz.ean.pojo.dto.DomainResponse;
 import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.DefaultRedirectStrategy;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.function.Supplier;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class TargetProductIntegrationService {
@@ -41,5 +57,7 @@ public class TargetProductIntegrationService {
         this.restTemplate = restTemplateSupplier.get();
         this.objectMapper = objectMapper;
         this.entityMapper = entityMapper;
+
+        this.createAnInstance();
     }
 }
