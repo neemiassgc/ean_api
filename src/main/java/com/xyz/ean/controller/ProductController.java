dc7a004fc2d55c69e46fce08561be0819d0b6ef4
Neemias Santos <neemiassgc@outlook.com>
2022-03-03 22:14:04 -0300

    Add End-Point POST api/products to the ProductController

--- src/main/java/com/xyz/ean/controller/ProductController.java
+++ src/main/java/com/xyz/ean/controller/ProductController.java
@@ -1,9 +1,15 @@
 package com.xyz.ean.controller;
 
+import com.xyz.ean.dto.EanCodeRequestDTO;
+import com.xyz.ean.dto.ProductResponseDTO;
+import com.xyz.ean.entity.Product;
 import com.xyz.ean.service.DomainMapper;
 import com.xyz.ean.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,3 +20,10 @@ public class ProductController {
 
     private final ProductService productService;
     private final DomainMapper domainMapper;
+
+    @PostMapping(path = "/products", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ProductResponseDTO createProduct(@RequestBody EanCodeRequestDTO eanCodeRequestDTO) {
+        final Product fetchedProduct = productService.saveByEanCode(eanCodeRequestDTO.getEanCode());
+        return domainMapper.mapToDto(fetchedProduct);
+    }
+}
