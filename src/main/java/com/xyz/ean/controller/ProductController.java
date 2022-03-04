cf0cd225bb0a2ee5927a458ee3379166e6fa3fb2
Neemias Santos <neemiassgc@outlook.com>
2022-03-04 09:26:07 -0300

    Add the handleException method for error handling to the ProductController

--- src/main/java/com/xyz/ean/controller/ProductController.java
+++ src/main/java/com/xyz/ean/controller/ProductController.java
@@ -3,15 +3,15 @@ package com.xyz.ean.controller;
 import com.xyz.ean.dto.EanCodeRequestDTO;
 import com.xyz.ean.dto.ProductResponseDTO;
 import com.xyz.ean.entity.Product;
+import com.xyz.ean.error.ErrorTemplate;
 import com.xyz.ean.service.DomainMapper;
 import com.xyz.ean.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping(path = "/api")
@@ -25,5 +25,15 @@ public class ProductController {
     public ProductResponseDTO createProduct(@RequestBody EanCodeRequestDTO eanCodeRequestDTO) {
         final Product fetchedProduct = productService.saveByEanCode(eanCodeRequestDTO.getEanCode());
         return domainMapper.mapToDto(fetchedProduct);
+    }
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<ErrorTemplate> handleException(final ResponseStatusException rse) {
+        final ErrorTemplate errorTemplate = new ErrorTemplate(rse.getStatus());
+        errorTemplate.addReasons(rse.getReason());
+
+        return ResponseEntity
+            .status(rse.getStatus())
+            .body(errorTemplate);
     }
 }
