61363bfc2290d2649c2f67aa492a4bd511891195
Neemias Santos <neemiassgc@outlook.com>
2022-03-03 22:06:24 -0300

    Initialize ProductService service class with its needed beans

--- src/main/java/com/xyz/ean/service/ProductService.java
+++ src/main/java/com/xyz/ean/service/ProductService.java
@@ -1,4 +1,14 @@
 package com.xyz.ean.service;
 
+import com.xyz.ean.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
 public class ProductService {
+
+    private final ProductRepository  productRepository;
+    private final ForeignProductHttpService foreignProductHttpService;
 }
