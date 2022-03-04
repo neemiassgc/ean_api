f0f88e7c2ef8cdb4ded9a1f2592bb64fd0f76ace
Neemias Santos <neemiassgc@outlook.com>
2022-03-04 12:26:43 -0300

    Add a JPQL query: findByEanCode, to the ProductRepository

--- src/main/java/com/xyz/ean/repository/ProductRepository.java
+++ src/main/java/com/xyz/ean/repository/ProductRepository.java
@@ -2,8 +2,13 @@ package com.xyz.ean.repository;
 
 import com.xyz.ean.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ProductRepository extends JpaRepository<Product, UUID> {
+
+    @Query("SELECT pro, pri FROM Product pro JOIN FETCH pro.prices pri WHERE pro.eanCode = ?1")
+    Optional<Product> findByEanCode(final String eanCode);
 }
