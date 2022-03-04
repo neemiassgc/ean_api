fd983e9ba5da975b1f40a8ccb8606aff7641c696
Neemias Santos <neemiassgc@outlook.com>
2022-03-03 22:03:08 -0300

    Add a new DTO: EanCodeRequestDTO

---
+++ src/main/java/com/xyz/ean/dto/EanCodeRequestDTO.java
@@ -0,0 +1,13 @@
+package com.xyz.ean.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class EanCodeRequestDTO {
+
+    private String eanCode;
+}
