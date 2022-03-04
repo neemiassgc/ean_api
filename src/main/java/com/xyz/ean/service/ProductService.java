518beb0ca5ca64ebc55dec72160b6f28b3845170
Neemias Santos <neemiassgc@outlook.com>
2022-03-03 22:07:31 -0300

    Add the saveByEanCode method to the ProductService service class

--- src/main/java/com/xyz/ean/service/ProductService.java
+++ src/main/java/com/xyz/ean/service/ProductService.java
@@ -1,6 +1,8 @@
 package com.xyz.ean.service;
 
+import com.xyz.ean.entity.Product;
 import com.xyz.ean.repository.ProductRepository;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,4 +13,11 @@ public class ProductService {
 
     private final ProductRepository  productRepository;
     private final ForeignProductHttpService foreignProductHttpService;
+
+    public Product saveByEanCode(@NonNull final String eanCode) {
+        final Product product = foreignProductHttpService.fetchByEanCode(eanCode)
+            .orElseThrow(() -> new IllegalStateException("Product not found"));
+
+        return productRepository.save(product);
+    }
 }
