8ce55dad63aa09478fa1de73ff6d03a73a8e22e8
Neemias Santos <neemiassgc@outlook.com>
2022-03-14 03:43:30 +0100

    Initialize ProductServiceTest unit test

--- src/test/java/com/xyz/ean/service/ProductServiceTest.java
+++ src/test/java/com/xyz/ean/service/ProductServiceTest.java
@@ -1,5 +1,23 @@
 package com.xyz.ean.service;
 
+import com.xyz.ean.repository.ProductRepository;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.mockito.BDDMockito.mock;
+
 class ProductServiceTest {
+
+    private ProductService productServiceUnderTest;
+    private ForeignProductHttpService foreignProductHttpServiceMock;
+    private DomainMapper domainMapperMock;
+    private ProductRepository productRepositoryMock;
+
+    @BeforeEach
+    void setUp() {
+        this.foreignProductHttpServiceMock = mock(ForeignProductHttpService.class);
+        this.domainMapperMock = mock(DomainMapper.class);
+        this.productRepositoryMock = mock(ProductRepository.class);
+        this.productServiceUnderTest = new ProductService(this.productRepositoryMock, this.foreignProductHttpServiceMock, this.domainMapperMock);
+    }
 
 }
