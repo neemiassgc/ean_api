407997e18c3c1cd6f1cad67dba8a9f4164e01045
Neemias Santos <neemiassgc@outlook.com>
2022-03-30 16:12:25 +0100

    Add a new test method whenAjaxIdentifierIsNotFoundShouldThrowAnException to the ForeignProductHttpServiceTest

--- src/test/java/com/xyz/ean/service/ForeignProductHttpServiceTest.java
+++ src/test/java/com/xyz/ean/service/ForeignProductHttpServiceTest.java
@@ -20,6 +20,7 @@ import java.util.stream.Collectors;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willReturn;
 import static org.mockito.Mockito.*;
 
 public class ForeignProductHttpServiceTest {
@@ -115,5 +116,29 @@ public class ForeignProductHttpServiceTest {
         verify(this.restTemplateMock, times(1)).execute(anyString(), eq(HttpMethod.GET), isNull(), any(ResponseExtractor.class));
         verify(this.restTemplateMock, only()).execute(anyString(), eq(HttpMethod.GET), isNull(), any(ResponseExtractor.class));
         verify(this.restTemplateMock, never()).postForEntity(anyString(), anyMap(), eq(String.class));
+    }
+
+    @Test
+    void whenAjaxIdentifierIsNotFoundShouldThrowAnException() {
+        // given
+        given(this.restTemplateMock.execute(
+                eq("/f?p=171"), eq(HttpMethod.GET), isNull(), any(ResponseExtractor.class)
+        )).willReturn(Jsoup.parse(this.getDefaultHtmlContent()));
+
+        given(this.restTemplateMock.postForEntity(eq("/wwv_flow.accept"), anyMap(), eq(String.class))).will(invocation -> null);
+
+        given(this.restTemplateMock.execute(eq("/f?p=171:2:54321:NEXT:NO:2:P2_CURSOR:B"), eq(HttpMethod.GET), isNull(), any(ResponseExtractor.class)
+        )).willThrow(new IllegalStateException("Ajax identifier not found"));
+
+        // when
+        final Throwable actualThrowable = catchThrowable(() -> this.foreignProductHttpServiceUnderTest.getASessionInstance());
+
+        // then
+        assertThat(actualThrowable).isNotNull();
+        assertThat(actualThrowable).isInstanceOf(IllegalStateException.class);
+        assertThat(actualThrowable).hasMessage("Ajax identifier not found");
+
+        verify(this.restTemplateMock, times(2)).execute(anyString(), eq(HttpMethod.GET), isNull(), any(ResponseExtractor.class));
+        verify(this.restTemplateMock, times(1)).postForEntity(anyString(), anyMap(), eq(String.class));
     }
 }
