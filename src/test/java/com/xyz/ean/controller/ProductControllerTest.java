cea361213fac3b65448b934aaa1f3eeaf9881229
Neemias Santos <neemiassgc@outlook.com>
2022-04-02 15:08:38 +0100

    Add a unit test method: whenPOSTANonExistentEanCodeThenResponseNotFound, to the ProductController class

--- src/test/java/com/xyz/ean/controller/ProductControllerTest.java
+++ src/test/java/com/xyz/ean/controller/ProductControllerTest.java
@@ -7,20 +7,24 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.Instant;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.isNull;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 @WebMvcTest(ProductController.class)
@@ -39,14 +43,6 @@ class ProductControllerTest {
         this.mockMvc = standaloneSetup(new ProductController(this.productServiceMock, this.domainMapperMock)).build();
     }
 
-    private static ProductResponseDTO getProductResponseDTO() {
-        final ProductResponseDTO productResponseDTO = new ProductResponseDTO();
-        productResponseDTO.setDescription("default description");
-        productResponseDTO.setPrices(List.of(new ProductResponseDTO.PriceInstant(Instant.now(), 4.55)));
-        productResponseDTO.setEanCode("1234567890123");
-        productResponseDTO.setSequenceCode(12345);
-        return productResponseDTO;
-    }
     private static ProductResponseDTO getProductResponseDTO() {
         final ProductResponseDTO productResponseDTO = new ProductResponseDTO();
         productResponseDTO.setDescription("default description");
@@ -74,6 +70,33 @@ class ProductControllerTest {
         .andExpect(jsonPath("$.prices[0].price").value(4.55))
         .andExpect(jsonPath("$.eanCode").value("1234567890123"))
         .andExpect(jsonPath("$.sequenceCode").value(12345));
+
+        verify(this.productServiceMock, times(1)).saveByEanCode(anyString());
+        verify(this.domainMapperMock, times(1)).mapToDto(isNull());
+    }
+
+    @Test
+    void whenPOSTANonExistentEanCodeThenResponseNotFound() throws Exception {
+        given(this.productServiceMock.saveByEanCode(anyString())).willReturn(null);
+        given(this.domainMapperMock.mapToDto(isNull())).willThrow(new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found"));
+
+        final MvcResult mvcResult = mockMvc.perform(post("/api/products")
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON)
+            .content("{\"eanCode\":\"1234567890124\"}")
+        )
+        .andExpect(status().isNotFound())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$.reasons").isArray())
+        .andExpect(jsonPath("$.reasons", hasSize(1)))
+        .andExpect(jsonPath("$.reasons[0]").value("Product not found"))
+        .andExpect(jsonPath("$.status").value("NOT_FOUND"))
+        .andReturn();
+
+        assertThat(mvcResult.getResolvedException()).isInstanceOf(ResponseStatusException.class);
+        assertThat((ResponseStatusException)mvcResult.getResolvedException()).extracting("reason").isEqualTo("Product not found");
+        assertThat((ResponseStatusException)mvcResult.getResolvedException()).extracting("status").isEqualTo(HttpStatus.NOT_FOUND);
+
 
         verify(this.productServiceMock, times(1)).saveByEanCode(anyString());
         verify(this.domainMapperMock, times(1)).mapToDto(isNull());
