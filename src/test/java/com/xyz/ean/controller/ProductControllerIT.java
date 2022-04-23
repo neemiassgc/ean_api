3da873b51857e452bb957bc1bfb28fed59db4fca
Neemias Santos <neemiassgc@outlook.com>
2022-04-23 16:58:05 +0100

    Create a new integration test class: ProductControllerIT, and setup it

---
+++ src/test/java/com/xyz/ean/controller/ProductControllerIT.java
@@ -0,0 +1,15 @@
+package com.xyz.ean.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ProductControllerIT {
+
+    @Autowired private MockMvc mockMvc;
+    @Autowired private ObjectMapper objectMapper;
+}
