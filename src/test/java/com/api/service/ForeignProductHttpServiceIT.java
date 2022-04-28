b76bb46c4fc47ea1f84c797bec192776af69d066
Neemias Santos <neemiassgc@outlook.com>
2022-04-28 11:56:09 +0100

    Add an integration test, given_an_existing_barcode_then_return_a_dto, to the ForeignProductHttpServiceIT test class

--- src/test/java/com/api/service/ForeignProductHttpServiceIT.java
+++ src/test/java/com/api/service/ForeignProductHttpServiceIT.java
@@ -1,10 +1,34 @@
 package com.api.service;
 
+import com.api.dto.InputItemDTO;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ForeignProductHttpServiceIT {
 
+    @Autowired private ForeignProductHttpService foreignProductHttpServiceUnderTest;
+
+    @Test
+    void given_an_existing_barcode_then_return_a_dto() {
+        // Give
+        String existingBarcode = "7891962057620";
+
+        // When
+        final Optional<InputItemDTO> actualResult = foreignProductHttpServiceUnderTest.fetchByEanCode(existingBarcode);
+
+        // Then
+        assertThat(actualResult).isNotNull();
+        assertThat(actualResult).isPresent();
+        assertThat(actualResult).get().extracting("description").isEqualTo("PAO BAUDUC 400G INTE");
+        assertThat(actualResult).get().extracting("eanCode").isEqualTo(existingBarcode);
+        assertThat(actualResult).get().extracting("sequence").isEqualTo(134262);
+    }
 }
