efc8cbe59cba6a43cbd1a2a67fed212e3f94bdb4
Neemias Santos <neemiassgc@outlook.com>
2022-05-06 15:07:36 +0100

    Add validation annotations to the BarcodeRequestDTO class

--- src/main/java/com/api/dto/BarcodeRequestDTO.java
+++ src/main/java/com/api/dto/BarcodeRequestDTO.java
@@ -4,10 +4,17 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @Getter
 @ToString
 public final class BarcodeRequestDTO {
 
+    @NotBlank
+    @NotEmpty
+    @NotNull
     private final String barcode;
 
     @JsonCreator
