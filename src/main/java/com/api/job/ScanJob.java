393106816a4833584e77ad660eff738b102df939
Neemias Santos <neemiassgc@outlook.com>
2022-05-16 15:37:22 +0100

    Add loggers to the ScanJob job

--- src/main/java/com/api/job/ScanJob.java
+++ src/main/java/com/api/job/ScanJob.java
@@ -5,6 +5,7 @@ import com.api.entity.Product;
 import com.api.service.ProductExternalService;
 import com.api.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,7 @@ import java.util.Objects;
 
 @Component
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
+@Slf4j
 public class ScanJob implements Job {
 
     private final ProductExternalService productExternalService;
@@ -23,19 +25,25 @@ public class ScanJob implements Job {
     @Override
     public void execute(JobExecutionContext context) {
         final List<Product> productsToScan = productService.findAll();
+        log.info("ProductService.findAll() invoked; fetched {} products", productsToScan.size());
 
         productsToScan.forEach(product -> {
             final String productBarcode = product.getBarcode();
 
             productExternalService.fetchByEanCode(productBarcode).ifPresent(inputItem -> {
+                log.info("ProductExternalService.fetchByEanCode({}) invoked; ({})", productBarcode, inputItem.getDescription());
 
                 final Double currentPrice = inputItem.getCurrentPrice();
                 final Double lastPrice = product.getPrices().get(0).getPrice();
 
-                if (!Objects.equals(currentPrice, lastPrice)) product.addPrice(new Price(currentPrice));
+                if (!Objects.equals(currentPrice, lastPrice)) {
+                    product.addPrice(new Price(currentPrice));
+                    log.info("Product.addPrice({}) invoked; ({})", currentPrice, product.getDescription());
+                }
             });
         });
 
         productService.saveAll(productsToScan);
+        log.info("productService.saveAll(productsToScan) invoked");
     }
 }
