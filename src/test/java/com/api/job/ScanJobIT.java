2cea2549008a92148a00b797ea54b94fd69443d3
Neemias Santos <neemiassgc@outlook.com>
2022-05-16 15:40:32 +0100

    Refactor ScanJobIT's should_update_the_prices test to make it pass

--- src/test/java/com/api/job/ScanJobIT.java
+++ src/test/java/com/api/job/ScanJobIT.java
@@ -8,7 +8,10 @@ import org.quartz.JobExecutionContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -22,12 +25,16 @@ public class ScanJobIT {
     void should_add_prices() {
         scanJob.execute(Mockito.mock(JobExecutionContext.class));
 
-        final List<Product> actualProductList = productService.findAll();
+        final Map<UUID, Product> actualProductMap = productService
+            .findAll()
+            .stream()
+            .collect(Collectors.toMap(Product::getId, Function.identity()));
 
-        assertThat(actualProductList).hasSize(4);
-        assertThat(actualProductList.get(0).getPrices()).hasSize(5);
-        assertThat(actualProductList.get(1).getPrices()).hasSize(4);
-        assertThat(actualProductList.get(2).getPrices()).hasSize(3);
-        assertThat(actualProductList.get(3).getPrices()).hasSize(2);
+        assertThat(actualProductMap).hasSize(4);
+        assertThat(actualProductMap.get(UUID.fromString("7e49cbbf-0d4b-4a67-b108-346bef1c961f")).getPrices()).hasSize(5);
+        assertThat(actualProductMap.get(UUID.fromString("3f30dc5c-5ce1-4556-a648-de8e55b0f6be")).getPrices()).hasSize(4);
+        assertThat(actualProductMap.get(UUID.fromString("f5a3feed-b3f0-4253-99f9-129049856c4f")).getPrices()).hasSize(3);
+        assertThat(actualProductMap.get(UUID.fromString("f3a9f940-9c07-4986-a655-8b91119dae8a")).getPrices()).hasSize(2);
+
     }
 }
