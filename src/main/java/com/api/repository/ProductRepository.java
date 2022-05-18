2d9ac6514a753f753497c9b178353beea7e162c2
Neemias Santos <neemiassgc@outlook.com>
2022-05-18 16:02:31 +0100

    Add Spring SpEL - #{#entityName}, to the findByBarcode method's @Query annotation

--- src/main/java/com/api/repository/ProductRepository.java
+++ src/main/java/com/api/repository/ProductRepository.java
@@ -12,7 +12,7 @@ import java.util.UUID;
 
 public interface ProductRepository extends JpaRepository<Product, UUID> {
 
-    @Query("SELECT pro, pri FROM Product pro JOIN FETCH pro.prices pri WHERE pro.barcode = ?1 ORDER BY pri.instant DESC")
+    @Query("SELECT pro, pri FROM #{#entityName} pro JOIN FETCH pro.prices pri WHERE pro.barcode = ?1 ORDER BY pri.instant DESC")
     Optional<Product> findByBarcode(final String barcode);
 
     @Override
