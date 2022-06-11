8c0810d8161ac1238057c48bb6dd396fcb9e9f2e
Neemias Santos <neemiassgc@outlook.com>
2022-06-11 03:59:59 +0100

    Go

--- src/test/java/com/api/controller/ProductControllerIT.java
+++ src/test/java/com/api/controller/ProductControllerIT.java
@@ -67,7 +67,7 @@ public class ProductControllerIT {
             .andExpect(content().contentType(MediaType.APPLICATION_JSON))
             .andExpect(jsonPath("$").isArray())
             .andExpect(jsonPath("$", hasSize(4)))
-            .andExpect(jsonPath("$[*].priceInstants[0]", hasSize(4)))
+            .andExpect(jsonPath("$[*].priceInstants[0]", hasSize(0)))
             .andExpect(jsonPath("$[*].priceInstants[1]", hasSize(3)))
             .andExpect(jsonPath("$[*].priceInstants[2]", hasSize(2)))
             .andExpect(jsonPath("$[*].priceInstants[3]", hasSize(1)))
