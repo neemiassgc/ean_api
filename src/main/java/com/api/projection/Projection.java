3aeb085befa44281c0d3bde01d3744de20a7b723
Neemias Santos <neemiassgc@outlook.com>
2022-06-15 13:56:23 +0100

    Remove from Projection class the ProductWithLatestPrice interface projection

--- src/main/java/com/api/projection/Projection.java
+++ src/main/java/com/api/projection/Projection.java
@@ -1,16 +1,6 @@
 package com.api.projection;
 
-import java.util.UUID;
-
 public final class Projection {
 
     private Projection() {}
-
-    public interface ProductWithLatestPrice {
-        String getBarcode();
-        String getDesc();
-        String getSeqCode();
-        UUID getId();
-        double getLatestPrice();
-    }
 }
