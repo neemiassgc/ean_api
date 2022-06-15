de22c3c560a6929cd2d4b406fe6b4de348c1d619
Neemias Santos <neemiassgc@outlook.com>
2022-06-15 03:59:31 +0100

    Add a new projection to the Projection class - ProductWithLatestPrice

--- src/main/java/com/api/projection/Projection.java
+++ src/main/java/com/api/projection/Projection.java
@@ -1,6 +1,16 @@
 package com.api.projection;
 
+import java.util.UUID;
+
 public final class Projection {
 
     private Projection() {}
+
+    public interface ProductWithLatestPrice {
+        String getBarcode();
+        String getDesc();
+        String getSeqCode();
+        UUID getId();
+        double getLatestPrice();
+    }
 }
