42ae2c958773f4dc8c0a2b5c95f6911f94bcae75
Neemias Santos <neemiassgc@outlook.com>
2022-06-15 13:54:20 +0100

    Add PriceRepository a JPA method - findAllLatestPrice, with a JPQL query on it

--- src/main/java/com/api/repository/PriceRepository.java
+++ src/main/java/com/api/repository/PriceRepository.java
@@ -2,10 +2,15 @@ package com.api.repository;
 
 import com.api.entity.Price;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface PriceRepository extends JpaRepository<Price, UUID> {
+
+    @Query("SELECT p FROM Price p JOIN FETCH p.product WHERE p.instant IN (SELECT MAX(pr.instant) FROM Price pr GROUP BY pr.product)")
+    List<Price> findAllLatestPrice();
 }
