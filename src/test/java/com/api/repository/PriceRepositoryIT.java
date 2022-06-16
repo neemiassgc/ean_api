d6aebcdddf2e177742fd7c5155955b85e8707036
Neemias Santos <neemiassgc@outlook.com>
2022-06-16 20:17:22 +0100

    Add to the PriceRepositoryIT class a new test method - should_return_all_prices_with_their_products_findAll

--- src/test/java/com/api/repository/PriceRepositoryIT.java
+++ src/test/java/com/api/repository/PriceRepositoryIT.java
@@ -15,6 +15,8 @@ public class PriceRepositoryIT {
     @Autowired
     private PriceRepository priceRepository;
 
+    private static final int COUNT_OF_PRICES = 10;
+
     @Test
     void should_return_all_latest_prices_with_their_products_findAllLatestPrice() {
         final List<Price> latestPrices = priceRepository.findAllLatestPrice();
@@ -23,5 +25,14 @@ public class PriceRepositoryIT {
         assertThat(latestPrices).allSatisfy(price -> assertThat(price.getProduct()).isNotNull());
         assertThat(latestPrices).extracting("price", Double.class).containsExactly(5.65, 3.5, 6.49, 8.49);
 
+    }
+
+    @Test
+    void should_return_all_prices_with_their_products_findAll() {
+        final List<Price> prices = priceRepository.findAll();
+
+        assertThat(prices).isNotNull();
+        assertThat(prices).hasSize(COUNT_OF_PRICES);
+        assertThat(prices).allSatisfy(price -> assertThat(price.getProduct()).isNotNull());
     }
 }
