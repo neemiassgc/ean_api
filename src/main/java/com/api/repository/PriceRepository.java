1ba2eba582cdbb9dc0ff4ef80ac9ccd4a20b9564
Neemias Santos <neemiassgc@outlook.com>
2022-06-17 16:02:46 +0100

    Remove a query method from PriceRepository - findLatestPriceByProductBarcode

--- src/main/java/com/api/repository/PriceRepository.java
+++ src/main/java/com/api/repository/PriceRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
@@ -15,12 +14,6 @@ public interface PriceRepository extends JpaRepository<Price, UUID> {
 
     @Query("SELECT p FROM Price p JOIN FETCH p.product WHERE p.instant IN (SELECT MAX(pr.instant) FROM Price pr GROUP BY pr.product)")
     List<Price> findAllLatestPrice();
-
-    @Query(
-        "SELECT p FROM Price p JOIN FETCH p.product WHERE p.product.barcode = ?1 " +
-        "AND p.instant = (SELECT MAX(pr.instant) FROM Price pr WHERE pr.product.barcode = ?1 GROUP BY pr.product)"
-    )
-    Optional<Price> findLatestPriceByProductBarcode(@NonNull String barcode);
 
     @Query("SELECT p FROM Price p JOIN FETCH p.product WHERE p.product.barcode = ?1")
     List<Price> findAllByProductBarcode(@NonNull String barcode);
