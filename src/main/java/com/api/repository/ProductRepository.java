0fae93ac0b24e0ed5eab41ebb1ca61d8886abb36
Neemias Santos <neemiassgc@outlook.com>
2022-06-17 16:04:27 +0100

    Add new query method called findByBarcodeWithLatestPrice to the ProductRepository

--- src/main/java/com/api/repository/ProductRepository.java
+++ src/main/java/com/api/repository/ProductRepository.java
@@ -1,7 +1,9 @@
 package com.api.repository;
 
 import com.api.entity.Product;
+import com.api.projection.Projection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,6 +12,13 @@ import java.util.UUID;
 public interface ProductRepository extends JpaRepository<Product, UUID> {
 
     Optional<Product> findByBarcode(final String barcode);
+
+    @Query(
+        "SELECT pro.barcode AS barcode, pro.description AS description, pro.sequenceCode AS sequenceCode, " +
+        "pri.price AS latestPrice, pri.instant AS instant FROM Price pri JOIN pri.product pro WHERE pro.barcode = ?1 " +
+        "AND pri.instant = (SELECT MAX(pr.instant) FROM Price pr WHERE pr.product.barcode = ?1 GROUP BY pr.product)"
+    )
+    Optional<Projection.ProductWithLatestPrice> findByBarcodeWithLatestPrice(final String barcode);
 
     List<Product> findAllByOrderByDescriptionAsc();
 }
