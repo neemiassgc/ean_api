912b61176ef3b02e347f1e5cae468355c7c9c434
Neemias Santos <neemiassgc@outlook.com>
2022-06-18 12:21:10 +0100

    Add a persistence method called findProductByBarcode to the PersistenceService

--- src/main/java/com/api/service/PersistenceService.java
+++ src/main/java/com/api/service/PersistenceService.java
@@ -2,9 +2,16 @@ package com.api.service;
 
 import com.api.repository.PriceRepository;
 import com.api.repository.ProductRepository;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
+
+import static com.api.projection.Projection.ProductWithLatestPrice;
 
 @Service
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
@@ -12,4 +19,18 @@ public class PersistenceService {
 
     private final ProductRepository productRepository;
     private final PriceRepository priceRepository;
+    private final ProductExternalService productExternalService;
+    private final DomainMapper domainMapper;
+
+    public ProductWithLatestPrice findProductByBarcode(@NonNull final String barcode) {
+        return productRepository.findByBarcodeWithLatestPrice(barcode)
+            .or(() -> {
+                final Optional<ProductWithLatestPrice> optionalProductWithLatestPrice = productExternalService.fetchByEanCode(barcode);
+                optionalProductWithLatestPrice
+                    .ifPresent(productWithLatestPrice -> priceRepository.save(domainMapper.mapToPrice(productWithLatestPrice)));
+
+                return optionalProductWithLatestPrice;
+            })
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found"));
+    }
 }
