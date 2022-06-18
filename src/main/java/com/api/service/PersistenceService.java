249f118bd5e5cd7bd410d001ea1720786fe8055f
Neemias Santos <neemiassgc@outlook.com>
2022-06-18 15:40:19 +0100

    Refactor and generalize PersistenceService's persistence method - findProductByBarcode

--- src/main/java/com/api/service/PersistenceService.java
+++ src/main/java/com/api/service/PersistenceService.java
@@ -1,17 +1,19 @@
 package com.api.service;
 
+import com.api.entity.Price;
 import com.api.repository.PriceRepository;
 import com.api.repository.ProductRepository;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.Optional;
+import java.util.List;
 
-import static com.api.projection.Projection.ProductWithLatestPrice;
+import static com.api.projection.Projection.ProductBase;
 
 @Service
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
@@ -22,15 +24,18 @@ public class PersistenceService {
     private final ProductExternalService productExternalService;
     private final DomainMapper domainMapper;
 
-    public ProductWithLatestPrice findProductByBarcode(@NonNull final String barcode) {
-        return productRepository.findByBarcodeWithLatestPrice(barcode)
-            .or(() -> {
-                final Optional<ProductWithLatestPrice> optionalProductWithLatestPrice = productExternalService.fetchByEanCode(barcode);
-                optionalProductWithLatestPrice
-                    .ifPresent(productWithLatestPrice -> priceRepository.save(domainMapper.mapToPrice(productWithLatestPrice)));
+    @SuppressWarnings("unchecked")
+    public <P extends ProductBase> P findProductByBarcode(@NonNull final String barcode, int limit) {
+        final List<Price> priceList = priceRepository.findAllByProductBarcode(barcode, PageRequest.ofSize(limit));
 
-                return optionalProductWithLatestPrice;
-            })
+        if (!priceList.isEmpty())
+            return (P) domainMapper.toProductWithManyPrices(priceList);
+
+        final ProductBase productBase = productExternalService.fetchByEanCode(barcode)
             .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found"));
+
+        priceRepository.save(domainMapper.mapToPrice(productBase));
+
+        return (P) productBase;
     }
 }
