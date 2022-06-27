fdbcfc8ac6bd38749d5747682f0dad012d589459
Neemias Santos <neemiassgc@outlook.com>
2022-06-27 03:10:38 +0100

    Build the body of Violation class

--- src/main/java/com/api/error/Violation.java
+++ src/main/java/com/api/error/Violation.java
@@ -1,4 +1,17 @@
 package com.api.error;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public final class Violation {
+
+    private final String field;
+    private final String violationMessage;
+
+    @Override
+    public String toString() {
+        return String.format("'%s' %s", field, violationMessage);
+    }
 }
