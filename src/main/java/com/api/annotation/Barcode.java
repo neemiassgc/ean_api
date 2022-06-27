d33ac23c9b6be9dd4c885b91ccce51e34994acd0
Neemias Santos <neemiassgc@outlook.com>
2022-06-27 14:23:32 +0100

    Create a new validation annotation called Barcode

---
+++ src/main/java/com/api/annotation/Barcode.java
@@ -0,0 +1,29 @@
+package com.api.annotation;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Pattern(regexp = "\\d{13}", message = "barcode must contain only numbers")
+@Size(min = 13, max = 13, message = "barcode must has 13 characters")
+@NotBlank(message = "barcode must not be blank")
+@Target({ElementType.PARAMETER, ElementType.FIELD})
+@Retention(RUNTIME)
+@Constraint(validatedBy = {})
+@Documented
+public @interface Barcode {
+
+    String message() default "";
+
+    Class<?>[] groups() default { };
+
+    Class<? extends Payload>[] payload() default { };
+}
