a8c576d9252f7877d61642cd7ad4815b50f5027f
Neemias Santos <neemiassgc@outlook.com>
2022-06-29 15:43:24 +0100

    Refactor ProductControllerTest to reflect the new changes

--- src/test/java/com/api/controller/ProductControllerTest.java
+++ src/test/java/com/api/controller/ProductControllerTest.java
@@ -1,204 +1,150 @@
 package com.api.controller;
 
-import com.api.service.DomainMapper;
-import com.api.projection.ProductResponseDTO;
-import com.api.service.ProductService;
-import org.assertj.core.util.Objects;
+import com.api.service.PersistenceService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.validation.ValidationAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.web.server.ResponseStatusException;
 
-import java.time.Instant;
+import java.nio.charset.StandardCharsets;
 import java.util.Collections;
-import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.Matchers.hasSize;
-import static org.mockito.ArgumentMatchers.*;
+import static com.api.controller.ProductControllerTestUtils.getNewProduct;
+import static com.api.controller.ProductControllerTestUtils.getProductList;
+import static com.api.projection.Projection.ProductWithManyPrices;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 @WebMvcTest(ProductController.class)
+@Import(ValidationAutoConfiguration.class)
 class ProductControllerTest {
 
     @MockBean
-    private ProductService productServiceMock;
-
-    @SuppressWarnings("unused")
-    @MockBean
-    private DomainMapper domainMapperMock;
+    private PersistenceService persistenceService;
 
     private MockMvc mockMvc;
 
     @BeforeEach
     void setUp() {
-        this.mockMvc = standaloneSetup(new ProductController(this.productServiceMock, this.domainMapperMock))
-            .alwaysDo(print()).build();
-    }
-    
-    private ProductResponseDTO getANewInstanceOfResponseDTO() {
-        return ProductResponseDTO.builder()
-            .description("default description")
-            .priceInstants(List.of(new ProductResponseDTO.PriceInstant(Instant.now(), 4.55)))
-            .barcode("1234567890123")
-            .sequenceCode(12345).build();
+        this.mockMvc = standaloneSetup(new ProductController(this.persistenceService)).alwaysDo(print()).build();
     }
 
     @Test
-    void when_POST_an_existent_bar_code_then_response_200_create() throws Exception {
-        given(this.productServiceMock.saveByBarcode(anyString())).willReturn(null);
-        given(this.domainMapperMock.mapToDto(isNull())).willReturn(this.getANewInstanceOfResponseDTO());
-        final String existentBarCodeJson = "{\"eanCode\":\"1234567890123\"}";
+    void when_GET_getAll_should_response_all_products_with_200() throws Exception {
+        given(persistenceService.<ProductWithManyPrices>findAllProducts()).willReturn(getProductList());
 
-        mockMvc.perform(post("/api/products")
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .accept(MediaType.APPLICATION_JSON)
-            .content(existentBarCodeJson)
+        mockMvc.perform(get("/api/products")
             .characterEncoding("UTF-8")
+            .accept(MediaType.APPLICATION_JSON)
         )
         .andExpect(status().isOk())
         .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-        .andExpect(jsonPath("$.description").value("default description"))
-        .andExpect(jsonPath("$.priceInstants").isArray())
-        .andExpect(jsonPath("$.priceInstants").isNotEmpty())
-        .andExpect(jsonPath("$.priceInstants[0].priceValue").value(4.55))
-        .andExpect(jsonPath("$.barcode").value("1234567890123"))
-        .andExpect(jsonPath("$.sequenceCode").value(12345));
+        .andExpect(jsonPath("$").isArray())
+        .andExpect(jsonPath("$", hasSize(3)))
+        .andExpect(jsonPath("$[*].prices.*").isArray())
+        .andExpect(jsonPath("$[*].prices.*", hasSize(9)))
+        .andExpect(jsonPath("$[0].prices[0].price").value(5.45));
 
-        verify(this.productServiceMock, times(1)).saveByBarcode(anyString());
-        verify(this.domainMapperMock, times(1)).mapToDto(isNull());
+        verify(persistenceService, times(1)).findAllProducts();
+        verify(persistenceService, only()).findAllProducts();
     }
 
     @Test
-    void when_POST_a_non_existent_bar_code_then_response_404_create() throws Exception {
-        given(this.productServiceMock.saveByBarcode(anyString())).willThrow(new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found"));
-        given(this.domainMapperMock.mapToDto(isNull())).willReturn(null);
-        final String nonExistentBarCodeJson = "{\"eanCode\":\"1234567890123\"}";
+    void when_GET_getAll_should_response_a_empty_list_with_200() throws Exception {
+        given(persistenceService.findAllProducts()).willReturn(Collections.emptyList());
 
-        final MvcResult mvcResult = mockMvc.perform(post("/api/products")
+        mockMvc.perform(get("/api/products")
             .characterEncoding("UTF-8")
-            .contentType(MediaType.APPLICATION_JSON)
             .accept(MediaType.APPLICATION_JSON)
-            .content(nonExistentBarCodeJson)
         )
-        .andExpect(status().isNotFound())
+        .andExpect(status().isOk())
         .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-        .andExpect(jsonPath("$.reasons").isArray())
-        .andExpect(jsonPath("$.reasons", hasSize(1)))
-        .andExpect(jsonPath("$.reasons[0]").value("Product not found"))
-        .andExpect(jsonPath("$.status").value("NOT_FOUND"))
-        .andReturn();
+        .andExpect(jsonPath("$").isEmpty());
 
-        assertThat(mvcResult.getResolvedException()).isInstanceOf(ResponseStatusException.class);
-        assertThat((ResponseStatusException)mvcResult.getResolvedException()).extracting("reason").isEqualTo("Product not found");
-        assertThat((ResponseStatusException)mvcResult.getResolvedException()).extracting("status").isEqualTo(HttpStatus.NOT_FOUND);
-
-        verify(this.productServiceMock, times(1)).saveByBarcode(anyString());
-        verify(this.domainMapperMock, never()).mapToDto(isNull());
+        verify(persistenceService, times(1)).findAllProducts();
+        verify(persistenceService, only()).findAllProducts();
     }
 
     @Test
-    void if_there_are_products_available_then_response_them_with_200_getAll() throws Exception {
-        final List<ProductResponseDTO> productResponseDTOList = List.of(
-            this.getANewInstanceOfResponseDTO(),
-            this.getANewInstanceOfResponseDTO(),
-            this.getANewInstanceOfResponseDTO()
-        );
-        given(this.productServiceMock.findAllByOrderByDescriptionAsc()).willReturn(null);
-        given(this.domainMapperMock.mapToDtoList(isNull())).willReturn(productResponseDTOList);
+    void when_GET_getByBarcode_should_response_a_product_with_all_prices_with_200() throws Exception {
+        final String validBarcode = "7891962057620";
 
-        mockMvc.perform(get("/api/products").accept(MediaType.APPLICATION_JSON).characterEncoding("UTF-8"))
-            .andExpect(status().isOk())
-            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-            .andExpect(jsonPath("$").isArray())
-            .andExpect(jsonPath("$", hasSize(3)))
-            .andExpect(jsonPath("$[0].priceInstants").isArray())
-            .andExpect(jsonPath("$[0].priceInstants", hasSize(1)))
-            .andExpect(jsonPath("$[0].priceInstants[0].instant").exists())
-            .andExpect(jsonPath("$[0].priceInstants[0].priceValue").value(4.55));
+        given(persistenceService.findProductByBarcode(eq(validBarcode), eq(0))).willReturn(getNewProduct("5.45", "8.56", "6.3"));
 
-        verify(this.productServiceMock, times(1)).findAllByOrderByDescriptionAsc();
-        verify(this.domainMapperMock, times(1)).mapToDtoList(isNull());
-    }
-
-    @Test
-    void if_there_are_no_products_available_then_response_an_empty_list_with_200_getAll() throws Exception {
-        given(this.productServiceMock.findAllByOrderByDescriptionAsc()).willReturn(null);
-        given(this.domainMapperMock.mapToDtoList(isNull())).willReturn(Collections.emptyList());
-
-        mockMvc.perform(get("/api/products").accept(MediaType.APPLICATION_JSON).characterEncoding("UTF-8"))
-            .andExpect(status().isOk())
-            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-            .andExpect(jsonPath("$").isArray())
-            .andExpect(jsonPath("$", hasSize(0)));
-
-        verify(this.productServiceMock, times(1)).findAllByOrderByDescriptionAsc();
-        verify(this.domainMapperMock, times(1)).mapToDtoList(isNull());
-    }
-
-    @Test
-    void given_an_existent_bar_code_then_response_a_product_with_200_getByEanCode() throws Exception {
-        final String existentBarCode = "1234567890123";
-
-        given(this.productServiceMock.findByBarcode(eq(existentBarCode))).willReturn(null);
-        given(this.domainMapperMock.mapToDto(isNull())).willReturn(this.getANewInstanceOfResponseDTO());
-
-        mockMvc.perform(get("/api/products/"+existentBarCode).accept(MediaType.APPLICATION_JSON).characterEncoding("UTF-8"))
-            .andExpect(status().isOk())
-            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-            .andExpect(jsonPath("$.description").value("default description"))
-            .andExpect(jsonPath("$.priceInstants").isArray())
-            .andExpect(jsonPath("$.priceInstants").isNotEmpty())
-            .andExpect(jsonPath("$.priceInstants[0].priceValue").value(4.55))
-            .andExpect(jsonPath("$.barcode").value(existentBarCode))
-            .andExpect(jsonPath("$.sequenceCode").value(12345));
-
-        verify(this.productServiceMock, times(1)).findByBarcode(eq(existentBarCode));
-        verify(this.domainMapperMock, times(1)).mapToDto(isNull());
-    }
-
-    @Test
-    void given_a_non_existent_bar_code_then_response_400_getByEanCode() throws Exception {
-        final String nonExistentBarCode = "1234567890123";
-
-        given(this.productServiceMock.findByBarcode(eq(nonExistentBarCode))).willThrow(new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found"));
-        given(this.domainMapperMock.mapToDto(isNull())).willReturn(this.getANewInstanceOfResponseDTO());
-
-        final MvcResult mvcResult = mockMvc.perform(
-            get("/api/products/"+nonExistentBarCode)
+        mockMvc.perform(get("/api/products/"+validBarcode)
             .accept(MediaType.APPLICATION_JSON)
-            .characterEncoding("UTF-8")
+            .characterEncoding(StandardCharsets.UTF_8)
         )
-        .andExpect(jsonPath("$.reasons").isArray())
-        .andExpect(jsonPath("$.reasons", hasSize(1)))
-        .andExpect(jsonPath("$.reasons[0]").value("Product not found"))
-        .andExpect(jsonPath("$.status").value("NOT_FOUND"))
-        .andReturn();
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$").exists())
+        .andExpect(jsonPath("$.description", is("PAO 400G")))
+        .andExpect(jsonPath("$.sequenceCode", is(134262)))
+        .andExpect(jsonPath("$.barcode", is(validBarcode)))
+        .andExpect(jsonPath("$.prices", hasSize(3)))
+        .andExpect(jsonPath("$.prices[*].price", contains(5.45, 8.56, 6.3)));
 
-        assertThat(mvcResult.getResolvedException())
-            .isNotNull()
-            .isInstanceOf(ResponseStatusException.class);
+        verify(persistenceService, times(1)).findProductByBarcode(eq(validBarcode), eq(0));
+        verify(persistenceService, only()).findProductByBarcode(eq(validBarcode), eq(0));
+    }
 
-        assertThat(Objects.castIfBelongsToType(mvcResult.getResolvedException(), ResponseStatusException.class))
-            .satisfies(exception -> {
-                assertThat(exception.getStatus()).isEqualTo(HttpStatus.NOT_FOUND);
-                assertThat(exception.getReason()).isEqualTo("Product not found");
-            });
+    @Test
+    void when_GET_getByBarcode_should_response_a_product_with_latest_price() throws Exception {
+        final String validBarcode = "7891962057620";
+        final int limitOfPrices = 1;
 
-        verify(this.productServiceMock, times(1)).findByBarcode(eq(nonExistentBarCode));
-        verify(this.domainMapperMock, never()).mapToDto(isNull());
-        verify(this.productServiceMock, only()).findByBarcode(eq(nonExistentBarCode));
+        given(persistenceService.findProductByBarcode(eq(validBarcode), eq(limitOfPrices)))
+            .willReturn(getNewProduct());
 
+        mockMvc.perform(get("/api/products/"+validBarcode+"?lop="+limitOfPrices)
+            .accept(MediaType.APPLICATION_JSON)
+            .characterEncoding(StandardCharsets.UTF_8)
+        )
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$").exists())
+        .andExpect(jsonPath("$.description", is("PAO 400G")))
+        .andExpect(jsonPath("$.sequenceCode", is(134262)))
+        .andExpect(jsonPath("$.barcode", is(validBarcode)))
+        .andExpect(jsonPath("$.prices").doesNotExist())
+        .andExpect(jsonPath("$.latestPrice.price", is(2.6)));
+
+        verify(persistenceService, times(1)).findProductByBarcode(eq(validBarcode), eq(limitOfPrices));
+        verify(persistenceService, only()).findProductByBarcode(eq(validBarcode), eq(limitOfPrices));
+    }
+
+    @Test
+    void when_GET_getByBarcode_should_response_a_product_with_two_latest_prices() throws Exception {
+        final String validBarcode = "7891962057620";
+        final int limitOfPrices = 2;
+
+        given(persistenceService.findProductByBarcode(eq(validBarcode), eq(limitOfPrices))).willReturn(getNewProduct("2.6", "3.4"));
+
+        mockMvc.perform(get("/api/products/"+validBarcode+"?lop="+limitOfPrices)
+            .accept(MediaType.APPLICATION_JSON)
+            .characterEncoding(StandardCharsets.UTF_8)
+        )
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$").exists())
+        .andExpect(jsonPath("$.description", is("PAO 400G")))
+        .andExpect(jsonPath("$.sequenceCode", is(134262)))
+        .andExpect(jsonPath("$.barcode", is(validBarcode)))
+        .andExpect(jsonPath("$.prices", hasSize(2)))
+        .andExpect(jsonPath("$.prices[*].price", contains(2.6, 3.4)));
+
+        verify(persistenceService, times(1)).findProductByBarcode(eq(validBarcode), eq(limitOfPrices));
+        verify(persistenceService, only()).findProductByBarcode(eq(validBarcode), eq(limitOfPrices));
     }
 }
