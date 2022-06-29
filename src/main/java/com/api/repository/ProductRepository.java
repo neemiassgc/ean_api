bc529a4b33e83eb66409b76f5edcabafd8133233
Neemias Santos <neemiassgc@outlook.com>
2022-06-29 16:08:13 +0100

    Remove all unused query methods from ProductRepository

--- src/main/java/com/api/repository/ProductRepository.java
+++ src/main/java/com/api/repository/ProductRepository.java
@@ -1,24 +1,9 @@
 package com.api.repository;
 
 import com.api.entity.Product;
-import static com.api.projection.Projection.*;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 public interface ProductRepository extends JpaRepository<Product, UUID> {
-
-    Optional<Product> findByBarcode(final String barcode);
-
-    @Query(
-        "SELECT pro.barcode AS barcode, pro.description AS description, pro.sequenceCode AS sequenceCode, " +
-        "pri.price AS latestPrice, pri.instant AS instant FROM Price pri JOIN pri.product pro WHERE pro.barcode = ?1 " +
-        "AND pri.instant = (SELECT MAX(pr.instant) FROM Price pr WHERE pr.product.barcode = ?1 GROUP BY pr.product)"
-    )
-    Optional<ProductWithLatestPrice> findByBarcodeWithLatestPrice(final String barcode);
-
-    List<Product> findAllByOrderByDescriptionAsc();
 }
