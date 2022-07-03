b623131bb31703d2560d811596d8362e2fb39eb0
Neemias Santos <neemiassgc@outlook.com>
2022-07-03 19:50:24 +0100

    Create in the ProductRepository a query method named findAllId with a JPQL query over it

--- src/main/java/com/api/repository/ProductRepository.java
+++ src/main/java/com/api/repository/ProductRepository.java
@@ -1,9 +1,17 @@
 package com.api.repository;
 
 import com.api.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, UUID> {
+
+    @Query("SELECT p.id FROM Product p")
+    Page<UUID> findAllId(Pageable pageable);
 }
