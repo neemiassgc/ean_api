4d61dfdaeae9804adb48acb60a2f8514521e0405
Neemias Santos <neemiassgc@outlook.com>
2022-07-04 02:32:09 +0100

    Create in the ProductControllerTestUtils a test helper method named getPagedProductList

--- src/test/java/com/api/controller/ProductControllerTestUtils.java
+++ src/test/java/com/api/controller/ProductControllerTestUtils.java
@@ -1,8 +1,13 @@
 package com.api.controller;
 
+import static com.api.projection.Projection.*;
+
 import com.api.projection.Projection;
 import com.api.projection.ProjectionFactory;
 import org.assertj.core.util.Arrays;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 
 import java.math.BigDecimal;
 import java.time.Instant;
@@ -13,13 +18,13 @@ import java.util.List;
 class ProductControllerTestUtils {
 
     @SuppressWarnings("unchecked")
-    static <P extends Projection.ProductBase> P getNewProduct(final String... values) {
+    static <P extends ProductBase> P getNewProduct(final String... values) {
         if (Arrays.isNullOrEmpty(values)) {
             return (P) ProjectionFactory.productWithLatestPriceBuilder()
                 .barcode("7891962057620")
                 .description("PAO 400G")
                 .sequenceCode(134262)
-                .latestPrice(new Projection.PriceWithInstant(new BigDecimal("2.6"), Instant.now().minus(1, ChronoUnit.DAYS)))
+                .latestPrice(new PriceWithInstant(new BigDecimal("2.6"), Instant.now().minus(1, ChronoUnit.DAYS)))
                 .build();
         }
 
@@ -30,8 +35,8 @@ class ProductControllerTestUtils {
             .prices(
                 new AbstractList<>() {
                     @Override
-                    public Projection.PriceWithInstant get(int index) {
-                        return new Projection.PriceWithInstant(new BigDecimal(values[index]), Instant.now().minus(index + 1, ChronoUnit.DAYS));
+                    public PriceWithInstant get(int index) {
+                        return new PriceWithInstant(new BigDecimal(values[index]), Instant.now().minus(index + 1, ChronoUnit.DAYS));
                     }
 
                     @Override
@@ -43,11 +48,16 @@ class ProductControllerTestUtils {
             .build();
     }
 
-    static List<Projection.ProductWithManyPrices> getProductList() {
+    static List<ProductWithManyPrices> getProductList() {
         return List.of(
             getNewProduct("5.45", "3.67", "8.5"),
             getNewProduct("5.45", "3.67", "8.5"),
             getNewProduct("5.45", "3.67", "8.5")
         );
+    }
+
+    static Paged<List<ProductWithManyPrices>> getPagedProductList() {
+        Page<?> page = new PageImpl<>(List.of(1, 2, 3), PageRequest.of(0, 3), 3);
+        return ProjectionFactory.paged(page, getProductList());
     }
 }
