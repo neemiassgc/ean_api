f8767cd259ab4d6068f80960d2fb9a93b98fbad9
Neemias Santos <neemiassgc@outlook.com>
2022-07-05 15:57:08 +0100

    Add an integration test method called when_GET_getAll_should_response_the_first_page_of_paged_products_with_200 to the ProductControllerIT

--- src/test/java/com/api/controller/ProductControllerIT.java
+++ src/test/java/com/api/controller/ProductControllerIT.java
@@ -1,5 +1,7 @@
 package com.api.controller;
 
+import com.api.repository.PriceRepository;
+import com.api.repository.ProductRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.mockito.hamcrest.MockitoHamcrest;
@@ -10,6 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.nio.charset.StandardCharsets;
 
@@ -22,7 +25,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class ProductControllerIT {
 
     @Autowired private MockMvc mockMvc;
-    @Autowired private ObjectMapper objectMapper;
     
     private static final String DEFAULT_URL = "/api/products";
 
@@ -42,5 +44,28 @@ public class ProductControllerIT {
         .andExpect(jsonPath("$[*].prices[*].value", hasSize(66)))
         .andExpect(jsonPath("$[0].prices[*].value", contains(5.65, 9.9, 10.75, 7.5)))
         .andExpect(jsonPath("$[1].prices[*].value", contains(11.30, 6.57, 4.31, 7.04, 14.3, 13.40, 17.40, 12.00, 3.26, 17.7, 11.50)));
+    }
+
+    @Test
+    void when_GET_getAll_should_response_the_first_page_of_paged_products_with_200() throws Exception {
+        final String firstPage = "0-5";
+
+        mockMvc.perform(get(DEFAULT_URL).queryParam("pag", firstPage)
+            .characterEncoding(StandardCharsets.UTF_8)
+            .contentType(MediaType.APPLICATION_JSON)
+        )
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$.content").isArray())
+        .andExpect(jsonPath("$.content", hasSize(5)))
+        .andExpect(jsonPath("$.currentPage").value(0))
+        .andExpect(jsonPath("$.totalPages").value(3))
+        .andExpect(jsonPath("$.numberOfItems").value(5))
+        .andExpect(jsonPath("$.hasNext").value(true))
+        .andExpect(
+            jsonPath("$.content[*].barcode",
+            contains("7896336010058", "7891000055120", "7898279792299", "7891962047560", "7896045104482"))
+        )
+        .andExpect(jsonPath("$.content[*].prices[*]", hasSize(40)));
     }
 }
