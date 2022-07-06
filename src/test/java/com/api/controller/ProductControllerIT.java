b0114173ac7a3e4840c580e7cf7dd42124598496
Neemias Santos <neemiassgc@outlook.com>
2022-07-06 13:10:17 +0100

    Optimize imports in the ProductControllerIT

--- src/test/java/com/api/controller/ProductControllerIT.java
+++ src/test/java/com/api/controller/ProductControllerIT.java
@@ -1,21 +1,16 @@
 package com.api.controller;
 
-import com.api.repository.PriceRepository;
-import com.api.repository.ProductRepository;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.mockito.hamcrest.MockitoHamcrest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.nio.charset.StandardCharsets;
 
+import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
