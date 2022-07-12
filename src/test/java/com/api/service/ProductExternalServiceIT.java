de5cb27bb2e262b2a446682ee640249b4644ca65
Neemias Santos <neemiassgc@outlook.com>
2022-07-12 13:26:15 +0100

    Clean ProductExternalServiceIT class

--- src/test/java/com/api/service/ProductExternalServiceIT.java
+++ src/test/java/com/api/service/ProductExternalServiceIT.java
@@ -1,47 +1,15 @@
 package com.api.service;
 
-import com.api.projection.Projection;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.Optional;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductExternalServiceIT {
 
-    @Autowired private ProductExternalService productExternalServiceUnderTest;
+    @Autowired
+    private ProductExternalService productExternalServiceUnderTest;
 
-    @Test
-    void given_an_existing_barcode_then_return_a_projection() {
-        // Give
-        String existingBarcode = "7891962057620";
 
-        // When
-        final Optional<Projection.ProductWithLatestPrice> actualResult = productExternalServiceUnderTest.fetchByEanCode(existingBarcode);
-
-        // Then
-        assertThat(actualResult).isNotNull();
-        assertThat(actualResult).isPresent();
-        assertThat(actualResult).get().extracting("description").isEqualTo("PAO BAUDUC 400G INTE");
-        assertThat(actualResult).get().extracting("barcode").isEqualTo(existingBarcode);
-        assertThat(actualResult).get().extracting("sequenceCode").isEqualTo(134262);
-    }
-
-    @Test
-    void given_an_non_existing_barcode_then_return_an_empty_optional() {
-        // Give
-        String nonExistingBarcode = "7891962057621";
-
-        // When
-        final Optional<Projection.ProductWithLatestPrice> actualResult = productExternalServiceUnderTest.fetchByEanCode(nonExistingBarcode);
-
-        // Then
-        assertThat(actualResult).isNotNull();
-        assertThat(actualResult).isEmpty();
-    }
 }
