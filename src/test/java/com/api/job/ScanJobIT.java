70d1013c2c2d575db523b87b54e4eda8dcc730e0
Neemias Santos <neemiassgc@outlook.com>
2022-07-13 14:55:53 +0100

    Add to the ScanJobIT an unique integration test method called should_save_different_unequal_prices

--- src/test/java/com/api/job/ScanJobIT.java
+++ src/test/java/com/api/job/ScanJobIT.java
@@ -1,40 +1,26 @@
 package com.api.job;
 
-import com.api.entity.Product;
-import com.api.service.ProductService;
+import com.api.repository.PriceRepository;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import org.quartz.JobExecutionContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.Map;
-import java.util.UUID;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ScanJobIT {
 
-    @Autowired private ProductService productService;
     @Autowired private ScanJob scanJob;
+    @Autowired private PriceRepository priceRepository;
+
+    private final int AMOUNT_OF_PRICES = 67;
 
     @Test
-    void should_update_the_prices() {
-        scanJob.execute(Mockito.mock(JobExecutionContext.class));
+    void should_save_different_unequal_prices() {
+        scanJob.execute(null);
 
-        final Map<UUID, Product> actualProductMap = productService
-            .findAllByOrderByDescriptionAsc()
-            .stream()
-            .collect(Collectors.toMap(Product::getId, Function.identity()));
+        final long actualAmountOfPrices = priceRepository.count();
 
-        assertThat(actualProductMap).hasSize(4);
-        assertThat(actualProductMap.get(UUID.fromString("7e49cbbf-0d4b-4a67-b108-346bef1c961f")).getPrices()).hasSize(5);
-        assertThat(actualProductMap.get(UUID.fromString("3f30dc5c-5ce1-4556-a648-de8e55b0f6be")).getPrices()).hasSize(4);
-        assertThat(actualProductMap.get(UUID.fromString("f5a3feed-b3f0-4253-99f9-129049856c4f")).getPrices()).hasSize(3);
-        assertThat(actualProductMap.get(UUID.fromString("f3a9f940-9c07-4986-a655-8b91119dae8a")).getPrices()).hasSize(2);
-
+        assertThat(actualAmountOfPrices).isGreaterThan(AMOUNT_OF_PRICES);
     }
 }
