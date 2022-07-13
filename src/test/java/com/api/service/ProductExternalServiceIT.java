2cee315d8db2aad8beebe86565afd6f645408ef1
Neemias Santos <neemiassgc@outlook.com>
2022-07-13 11:24:26 +0100

    Add to the ProductExternalServiceIT an integration test method called should_return_a_product

--- src/test/java/com/api/service/ProductExternalServiceIT.java
+++ src/test/java/com/api/service/ProductExternalServiceIT.java
@@ -1,8 +1,14 @@
 package com.api.service;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static com.api.projection.Projection.ProductWithLatestPrice;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -11,5 +17,18 @@ class ProductExternalServiceIT {
     @Autowired
     private ProductExternalService productExternalServiceUnderTest;
 
+    @Test
+    void should_return_a_product() {
+        final String existingBarcode = "7896336014230";
+        
+        final Optional<ProductWithLatestPrice> optionalProduct =
+            productExternalServiceUnderTest.fetchByBarcode(existingBarcode)
+                .map(p -> (ProductWithLatestPrice) p);
 
+        assertThat(optionalProduct).isNotNull();
+        assertThat(optionalProduct.isPresent()).isTrue();
+        assertThat(optionalProduct.get()).extracting("description").isEqualTo("PASTA AMENDOIM FIRST");
+        assertThat(optionalProduct.get()).extracting("barcode").isEqualTo(existingBarcode);
+        assertThat(optionalProduct.get()).extracting("sequenceCode").isEqualTo(137638);
+    }
 }
