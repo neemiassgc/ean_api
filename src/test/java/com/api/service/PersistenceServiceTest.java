b587e444840ff136e108b8ebb3ce8b58ad8a28c6
Neemias Santos <neemiassgc@outlook.com>
2022-07-14 19:58:24 +0100

    Add resources for testing to the PersistenceServiceTese

--- src/test/java/com/api/service/PersistenceServiceTest.java
+++ src/test/java/com/api/service/PersistenceServiceTest.java
@@ -1,14 +1,22 @@
 package com.api.service;
 
 import com.api.entity.Price;
-import static com.api.projection.Projection.*;
+import com.api.projection.ProjectionFactory;
 import com.api.repository.PriceRepository;
 import com.api.repository.ProductRepository;
 import org.junit.jupiter.api.*;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.api.projection.Projection.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -20,15 +28,58 @@ public class PersistenceServiceTest {
     private ProductExternalService productExternalService;
     private DomainMapper domainMapper;
 
+    // resources for testing
     private final String DEFAULT_BARCODE = "7891000055120";
+    private List<Price> pricesForTesting = null;
 
     @BeforeAll
     void init() {
+        this.pricesForTesting = getSomePrices();
+    }
+
+    @BeforeEach
+    void preset() {
         this.priceRepository = mock(PriceRepository.class);
         this.productRepository = mock(ProductRepository.class);
         this.productExternalService = mock(ProductExternalService.class);
         this.domainMapper = mock(DomainMapper.class);
-        persistenceServiceUnderTest =  new PersistenceService(productRepository, priceRepository, productExternalService, domainMapper);
+        this.persistenceServiceUnderTest =  new PersistenceService(productRepository, priceRepository, productExternalService, domainMapper);
+    }
+
+    private ProductWithManyPrices getDefaultProductWithManyPrices(final List<Price> priceList) {
+        return ProjectionFactory.productWithManyPricesBuilder()
+            .description("A product")
+            .barcode(DEFAULT_BARCODE)
+            .sequenceCode(12345)
+            .prices(priceList
+                .stream()
+                .map(it -> new PriceWithInstant(it.getValue(), it.getInstant()))
+                .collect(Collectors.toList())
+            )
+            .build();
+    }
+
+    private List<Price> getSomePrices() {
+        final List<Price> pricesToReturn = new ArrayList<>(15);
+
+        pricesToReturn.add(new Price(new BigDecimal("59.90"), null));
+        pricesToReturn.add(new Price(new BigDecimal("18.37"), null));
+        pricesToReturn.add(new Price(new BigDecimal("24.90"), null));
+        pricesToReturn.add(new Price(new BigDecimal("21.62"), null));
+        pricesToReturn.add(new Price(new BigDecimal("21.16"), null));
+        pricesToReturn.add(new Price(new BigDecimal("7.46"), null));
+        pricesToReturn.add(new Price(new BigDecimal("54.21"), null));
+        pricesToReturn.add(new Price(new BigDecimal("41.80"), null));
+        pricesToReturn.add(new Price(new BigDecimal("6.29"), null));
+        pricesToReturn.add(new Price(new BigDecimal("9.50"), null));
+        pricesToReturn.add(new Price(new BigDecimal("45.87"), null));
+        pricesToReturn.add(new Price(new BigDecimal("26.30"), null));
+        pricesToReturn.add(new Price(new BigDecimal("20.38"), null));
+        pricesToReturn.add(new Price(new BigDecimal("19.18"), null));
+        pricesToReturn.add(new Price(new BigDecimal("33.40"), null));
+        pricesToReturn.add(new Price(new BigDecimal("15.66"), null));
+
+        return pricesToReturn;
     }
 
     @Nested
