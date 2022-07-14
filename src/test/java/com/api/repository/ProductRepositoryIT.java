58f6d6ed84dcc6cc6cf88cbc46fadf0621052afc
Neemias Santos <neemiassgc@outlook.com>
2022-07-14 12:52:30 +0100

    Add to the ProductRepositoryIT an integration test method called should_return_all_ids_findAllId

--- src/test/java/com/api/repository/ProductRepositoryIT.java
+++ src/test/java/com/api/repository/ProductRepositoryIT.java
@@ -1,7 +1,28 @@
 package com.api.repository;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 public class ProductRepositoryIT {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Test
+    void should_return_all_ids_findAllId() {
+        final Page<UUID> uuidPage = productRepository.findAllId(PageRequest.ofSize(10));
+
+        assertThat(uuidPage).isNotNull();
+        assertThat(uuidPage.getContent()).isInstanceOf(List.class);
+        assertThat(uuidPage.getContent()).hasSize(10);
+    }
 }
