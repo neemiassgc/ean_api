432ab03925e677990e0e62fbebd0c4e4b0cb22f2
Neemias Santos <neemiassgc@outlook.com>
2022-07-14 14:46:22 +0100

    Setup PersistenceServiceTest

--- src/test/java/com/api/service/PersistenceServiceTest.java
+++ src/test/java/com/api/service/PersistenceServiceTest.java
@@ -1,4 +1,27 @@
 package com.api.service;
 
+import com.api.repository.PriceRepository;
+import com.api.repository.ProductRepository;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
+
+import static org.mockito.Mockito.mock;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class PersistenceServiceTest {
+
+    private PersistenceService persistenceServiceUnderTest;
+    private PriceRepository priceRepository;
+    private ProductRepository productRepository;
+    private ProductExternalService productExternalService;
+    private DomainMapper domainMapper;
+
+    @BeforeAll
+    void init() {
+        this.priceRepository = mock(PriceRepository.class);
+        this.productRepository = mock(ProductRepository.class);
+        this.productExternalService = mock(ProductExternalService.class);
+        this.domainMapper = mock(DomainMapper.class);
+        persistenceServiceUnderTest =  new PersistenceService(productRepository, priceRepository, productExternalService, domainMapper);
+    }
 }
