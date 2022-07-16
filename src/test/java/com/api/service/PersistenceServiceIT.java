7110f17d3b4f05aa496d24d37fda166e84a36129
Neemias Santos <neemiassgc@outlook.com>
2022-07-16 14:44:19 +0100

    Create a new integration test class called PersistenceServiceIT and setup it

---
+++ src/test/java/com/api/service/PersistenceServiceIT.java
@@ -0,0 +1,11 @@
+package com.api.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PersistenceServiceIT {
+
+    @Autowired
+    private PersistenceService persistenceService;
+}
