1fce3fa2fed570a5f447ec00ad31a5817bddf21c
Neemias Santos <neemiassgc@outlook.com>
2022-07-20 14:27:50 +0100

    Add an integration test method called should_return_all_the_products to the PersistenceServiceIT class

--- src/test/java/com/api/service/PersistenceServiceIT.java
+++ src/test/java/com/api/service/PersistenceServiceIT.java
@@ -15,6 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @SpringBootTest
 @Transactional
@@ -104,5 +105,26 @@ public class PersistenceServiceIT {
                 assertThat(exception.getStatus()).isEqualTo(HttpStatus.NOT_FOUND);
             });
         }
+    }
+
+    @DisplayName("Testing findAllProducts method")
+    @Test
+    void should_return_all_the_products() {
+        final List<ProductWithManyPrices> actualProducts = persistenceServiceUnderTest.findAllProducts();
+
+        assertThat(actualProducts).hasSize(11);
+        assertThat(actualProducts).flatExtracting(ProductWithManyPrices::getPrices).hasSize(66);
+
+        // verify order of the products
+        assertThat(actualProducts).extracting(ProductWithManyPrices::getBarcode)
+            .containsExactly(
+                "7891000055120", "7897534852624", "7896336010058", "7898279792299", "7896045104482",
+                "7891962047560", "7896656800018", "7896004004501", "7891098010575", "7896036093085", "7891962057620"
+            );
+
+        // verify order of the prices
+        assertThat(actualProducts.get(0).getPrices().stream().mapToDouble(it -> it.getValue().doubleValue()))
+            .containsExactly(12.7, 19.0, 16.5, 6.61, 16.8, 9.85, 10.6, 16.1, 12.6, 19.1);
+
     }
 }
