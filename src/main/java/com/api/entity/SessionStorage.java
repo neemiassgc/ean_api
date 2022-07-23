d233f9f86be691a6563da1c7b4f36c94e544ea6c
Neemias Santos <neemiassgc@outlook.com>
2022-07-23 13:11:40 +0100

    Crate a new entity called SessionStorage

---
+++ src/main/java/com/api/entity/SessionStorage.java
@@ -0,0 +1,37 @@
+package com.api.entity;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SessionStorage {
+
+    @Id
+    private Long instance;
+
+    @Column(length = 100)
+    private String cookieKey;
+
+    @Column(length = 100)
+    private String cookieValue;
+
+    private String ajaxId;
+
+    private LocalDate creationDate;
+
+    @Override
+    public String toString() {
+        return String.format(
+            "SessionStorage{instance=%s, cookieKey=%s, cookieValue=%s, ajaxId=%s, creationDate=%s}",
+            instance, cookieKey, cookieValue, ajaxId, creationDate
+        );
+    }
+}
