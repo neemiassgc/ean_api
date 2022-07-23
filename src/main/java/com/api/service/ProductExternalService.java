3c128d8701dc7cea3c0479788bc7e45c186d70ef
Neemias Santos <neemiassgc@outlook.com>
2022-07-23 13:31:28 +0100

    Refactor ProductExternalService class for being able to store and reuse a session

--- src/main/java/com/api/service/ProductExternalService.java
+++ src/main/java/com/api/service/ProductExternalService.java
@@ -1,24 +1,29 @@
 package com.api.service;
 
+import com.api.entity.SessionStorage;
 import com.api.pojo.DomainUtils;
 import com.api.pojo.SessionInstance;
 import com.api.projection.Projection;
+import com.api.repository.SessionStorageRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.exc.InvalidDefinitionException;
 import lombok.AccessLevel;
 import lombok.Setter;
+import lombok.extern.log4j.Log4j2;
+import org.apache.http.HttpRequestInterceptor;
+import org.apache.http.client.CookieStore;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.DefaultRedirectStrategy;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.cookie.BasicClientCookie;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
+import org.springframework.data.util.Pair;
+import org.springframework.http.*;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
@@ -26,23 +31,54 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 import static com.api.projection.Projection.ProductBase;
 
 @Service
+@Log4j2
 public class ProductExternalService {
 
     private final RestTemplate restTemplate;
     private final ObjectMapper objectMapper;
+    private final SessionStorageRepository sessionStorageRepository;
 
     @Setter(AccessLevel.PRIVATE)
     private SessionInstance sessionInstance;
 
     @Autowired
-    public ProductExternalService(final RestTemplateBuilder restTemplateBuilder, final ObjectMapper objectMapper) {
+    public ProductExternalService(
+        final RestTemplateBuilder restTemplateBuilder,
+        final ObjectMapper objectMapper,
+        final SessionStorageRepository sessionStorageRepository
+    ) {
+        this.objectMapper = objectMapper;
+        this.sessionStorageRepository = sessionStorageRepository;
+
+        final CookieStore basicCookieStore = new BasicCookieStore();
+
+        final SessionStorage actualSession =
+            sessionStorageRepository.findTopByOrderByCreationDateDesc().orElseThrow();
+
+        if (actualSession.getCreationDate().isEqual(LocalDate.now())) {
+            log.info("Reusing session from DB");
+            this.sessionInstance = new SessionInstance(actualSession.getInstance() + "", actualSession.getAjaxId());
+
+            final BasicClientCookie cookie = new BasicClientCookie(actualSession.getCookieKey(), actualSession.getCookieValue());
+            cookie.setPath("/");
+            cookie.setSecure(true);
+            cookie.setDomain("apex.savegnago.com.br");
+            cookie.setExpiryDate(Date.from(Instant.now().plus(10, ChronoUnit.DAYS)));
+            basicCookieStore.addCookie(cookie);
+        }
+        else this.sessionInstance = SessionInstance.EMPTY_SESSION;
+
         final int twoSeconds = (int) Duration.ofSeconds(2).toMillis();
         final RequestConfig requestConfig = RequestConfig
             .custom()
@@ -51,25 +87,23 @@ public class ProductExternalService {
             .setConnectTimeout(twoSeconds)
             .build();
 
-        final CloseableHttpClient httpClient =
-            HttpClientBuilder.create()
-                .setDefaultRequestConfig(requestConfig)
-                .setRedirectStrategy(DefaultRedirectStrategy.INSTANCE)
-                .setDefaultCookieStore(new BasicCookieStore())
-                .build();
+        final CloseableHttpClient httpClient = HttpClientBuilder.create()
+            .setDefaultRequestConfig(requestConfig)
+            .setRedirectStrategy(DefaultRedirectStrategy.INSTANCE)
+            .setDefaultCookieStore(basicCookieStore)
+            .build();
 
         this.restTemplate = restTemplateBuilder
             .rootUri("https://apex.savegnago.com.br/apexmobile")
             .requestFactory(() -> new HttpComponentsClientHttpRequestFactory(httpClient))
             .build();
-
-        this.objectMapper = objectMapper;
-        this.sessionInstance = SessionInstance.EMPTY_SESSION;
     }
 
     private Map<String, String> initialScrapingRequest() {
         final Map<String, String> resourcesToReturn = new HashMap<>(3);
 
+        // Checking for resources from the login page....
+        log.info("Scraping login page to get form fields");
         final Document loginPageDocument = DomainUtils.requireNonNull(
             this.restTemplate.execute(
                 "/f?p=171",
@@ -102,12 +136,14 @@ public class ProductExternalService {
         return resourcesToReturn;
     }
 
-    private String loginRequest(final Map<String, String> resourcesMap) {
+    private Pair<String, String> loginRequest(final Map<String, String> resourcesMap) {
         assert resourcesMap != null;
         assert resourcesMap.containsKey("instance_id");
         assert resourcesMap.containsKey("submission_id");
         assert resourcesMap.containsKey("checksum");
 
+        // Login with the credentials..
+        log.info("Creating a request body for a POST request using "+MediaType.APPLICATION_FORM_URLENCODED_VALUE+" as media type");
         final MultiValueMap<String, String> requestBody = new LinkedMultiValueMap<>();
         requestBody.add("p_flow_id", "171");
         requestBody.add("p_flow_step_id", "101");
@@ -121,9 +157,15 @@ public class ProductExternalService {
         requestBody.add("p_page_checksum", resourcesMap.get("checksum"));
 
         // It's just to get a cookie for the session
-        this.restTemplate.postForEntity("/wwv_flow.accept", requestBody, String.class);
+        log.info("Logging into the system to get a Cookie session and a AjaxId");
+        final ResponseEntity<?> responseEntity = this.restTemplate.postForEntity("/wwv_flow.accept", requestBody, String.class);
+        final String cookie = Objects.requireNonNull(responseEntity.getHeaders().get("Set-Cookie")).get(0);
 
-        return this.restTemplate.execute("/f?p=171:2:"+resourcesMap.get("instance_id")+":NEXT:NO:2:P2_CURSOR:B", HttpMethod.GET, null,
+        // Request to get the 'ajaxIdentifier'
+        final String ajaxId = this.restTemplate.execute(
+            "/f?p=171:2:"+resourcesMap.get("instance_id")+":NEXT:NO:2:P2_CURSOR:B",
+            HttpMethod.GET,
+            null,
             response -> {
                 final String html = DomainUtils.readFromInputStream(response.getBody());
 
@@ -132,13 +174,26 @@ public class ProductExternalService {
                 return mtc.group(1);
             }
         );
+
+        return Pair.of(Objects.requireNonNull(ajaxId), cookie);
     }
 
     public SessionInstance newSessionInstance() {
         final Map<String, String> resourcesMap = this.initialScrapingRequest();
-        final String ajaxIdentifier = this.loginRequest(resourcesMap);
+        final Pair<String, String> pairOfResources = this.loginRequest(resourcesMap);
 
-        return new SessionInstance(resourcesMap.get("instance_id"), ajaxIdentifier);
+        final String[] pairOfCookies = pairOfResources.getSecond().split("=");
+        final SessionStorage sessionStorage = new SessionStorage();
+        sessionStorage.setInstance(Long.parseLong(resourcesMap.get("instance_id"), 10));
+        sessionStorage.setAjaxId(pairOfResources.getFirst());
+        sessionStorage.setCookieKey(pairOfCookies[0]);
+        sessionStorage.setCookieValue(pairOfCookies[1]);
+        sessionStorage.setCreationDate(LocalDate.now());
+
+        log.info("Saving a new session in the DB");
+        sessionStorageRepository.save(sessionStorage);
+
+        return new SessionInstance(sessionStorage.getInstance()+"", pairOfResources.getFirst());
     }
 
     public Optional<ProductBase> fetchByBarcode(final String barcode) {
@@ -159,6 +214,7 @@ public class ProductExternalService {
 
         final HttpEntity<MultiValueMap<String, String>> httpEntity = new HttpEntity<>(body, headers);
 
+        log.info("Fetching for information about a product by barcode");
         return restTemplate.execute(
             "/wwv_flow.show",
             HttpMethod.POST,
