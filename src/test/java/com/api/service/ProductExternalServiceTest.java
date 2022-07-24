6ced53359e6825812e4d8db1276deeb08cbf2215
Neemias Santos <neemiassgc@outlook.com>
2022-07-24 20:46:47 +0100

    Refactor ProductExternalServiceTest's all the methods to sync with transitive changes

--- src/test/java/com/api/service/ProductExternalServiceTest.java
+++ src/test/java/com/api/service/ProductExternalServiceTest.java
@@ -1,33 +1,38 @@
 package com.api.service;
 
+import com.api.entity.SessionStorage;
 import com.api.pojo.SessionInstance;
 import com.api.projection.ProjectionFactory;
+import com.api.repository.SessionStorageRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.exc.InvalidDefinitionException;
 import org.jsoup.Jsoup;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
 import org.springframework.mock.http.client.MockClientHttpResponse;
 import org.springframework.web.client.ResponseExtractor;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.math.BigDecimal;
 import java.time.Instant;
+import java.time.LocalDate;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.function.Consumer;
 import java.util.function.Supplier;
-import java.util.stream.Collectors;
 
 import static com.api.projection.Projection.PriceWithInstant;
 import static com.api.projection.Projection.ProductWithLatestPrice;
+import static com.api.service.ProductExternalServiceTestTools.getGenericHtmlContent;
+import static com.api.service.ProductExternalServiceTestTools.newSessionStorage;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.mockito.BDDMockito.*;
@@ -37,10 +42,10 @@ public class ProductExternalServiceTest {
 
     private RestTemplate restTemplateMock;
     private ObjectMapper objectMapperMock;
+    private SessionStorageRepository sessionStorageRepository;
     private ProductExternalService productExternalServiceUnderTest;
 
-    @BeforeEach
-    void setUp() {
+    void setup(@Nullable final Consumer<SessionStorageRepository> consumer) {
         this.restTemplateMock = mock(RestTemplate.class);
 
         final RestTemplateBuilder restTemplateBuilderMock = mock(RestTemplateBuilder.class);
@@ -49,37 +54,46 @@ public class ProductExternalServiceTest {
         given(restTemplateBuilderMock.build()).willReturn(this.restTemplateMock);
 
         this.objectMapperMock = mock(ObjectMapper.class);
-        this.productExternalServiceUnderTest = new ProductExternalService(restTemplateBuilderMock, this.objectMapperMock);
+        this.sessionStorageRepository = mock(SessionStorageRepository.class);
+
+        if (Objects.nonNull(consumer)) consumer.accept(this.sessionStorageRepository);
+
+        this.productExternalServiceUnderTest =
+            new ProductExternalService(restTemplateBuilderMock, this.objectMapperMock, sessionStorageRepository);
     }
 
     private void reusableSessionInstance() {
+        // Parse the login page
         given(this.restTemplateMock.execute(
             eq("/f?p=171"), eq(HttpMethod.GET), isNull(), any(ResponseExtractor.class)
-        )).willReturn(Jsoup.parse(this.getGenericHtmlContent()));
+        )).willReturn(Jsoup.parse(getGenericHtmlContent(this)));
 
-        given(this.restTemplateMock.postForEntity(eq("/wwv_flow.accept"), anyMap(), eq(String.class))).will(invocation -> null);
+        // Login into the system to get a cookie
+        given(this.restTemplateMock.postForEntity(eq("/wwv_flow.accept"), anyMap(), eq(String.class)))
+            .willReturn(
+                ResponseEntity
+                    .ok()
+                    .header("Set-Cookie", "COOKIE_SAVEG_MOBILE=ORA_WWV-dxVoldWhIfN2TviPcS9yhmcI")
+                    .build()
+            );
 
+        // Parse page to get a Ajax Identifier
         given(this.restTemplateMock.execute(
             eq("/f?p=171:2:54321:NEXT:NO:2:P2_CURSOR:B"), eq(HttpMethod.GET), isNull(), any(ResponseExtractor.class)
         )).willReturn("peanut butter");
     }
 
-    private String getGenericHtmlContent() {
-        final InputStreamReader isr = new InputStreamReader(
-            Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream("html_for_test.html"))
-        );
-
-        try (BufferedReader br = new BufferedReader(isr)) {
-            return br.lines().collect(Collectors.joining("\n"));
-        }
-        catch (IOException ignored) {
-            return "";
-        }
-    }
-
     @Test
-    void if_the_structures_of_the_html_responses_are_intact_then_should_return_a_valid_session_instance__newSessionInstance() {
+    @DisplayName("If everything is ok then should return a valid session instance")
+    void should_return_a_valid_session_instance() {
         // given
+        setup(it -> {
+            given(it.findTopByOrderByCreationDateDesc()).willReturn(
+                Optional.of(newSessionStorage(LocalDate.now().minusDays(3)))
+            );
+            given(it.save(any(SessionStorage.class))).willAnswer(invocation -> invocation.getArgument(0));
+        });
+
         this.reusableSessionInstance();
 
         // when
@@ -92,11 +106,20 @@ public class ProductExternalServiceTest {
 
         verify(this.restTemplateMock, times(2)).execute(anyString(), eq(HttpMethod.GET), isNull(), any(ResponseExtractor.class));
         verify(this.restTemplateMock, times(1)).postForEntity(anyString(), anyMap(), eq(String.class));
+        verify(this.sessionStorageRepository, times(1)).findTopByOrderByCreationDateDesc();
+        verify(this.sessionStorageRepository, times(1)).save(any(SessionStorage.class));
     }
 
     @Test
-    void if_the_login_page_parsing_fails_then_should_throw_an_exception_newSessionInstance() {
+    @DisplayName("Should throw an exception - newSessionInstance")
+    void if_unable_possible_to_parse_the_login_page_then_should_throw_an_exception() {
         // given
+        setup(it -> {
+            given(it.findTopByOrderByCreationDateDesc()).willReturn(
+                Optional.of(newSessionStorage(LocalDate.now().minusDays(3)))
+            );
+        });
+
         given(this.restTemplateMock.execute(
             eq("/f?p=171"), eq(HttpMethod.GET), isNull(), any(ResponseExtractor.class)
         )).willReturn(null);
@@ -111,11 +134,20 @@ public class ProductExternalServiceTest {
         verify(this.restTemplateMock, times(1)).execute(eq("/f?p=171"), eq(HttpMethod.GET), isNull(), any(ResponseExtractor.class));
         verify(this.restTemplateMock, only()).execute(eq("/f?p=171"), eq(HttpMethod.GET), isNull(), any(ResponseExtractor.class));
         verify(this.restTemplateMock, never()).postForEntity(anyString(), anyMap(), eq(String.class));
+        verify(this.sessionStorageRepository, times(1)).findTopByOrderByCreationDateDesc();
+        verify(this.sessionStorageRepository, never()).save(any(SessionStorage.class));
     }
 
     @Test
-    void when_some_of_the_required_fields_are_missing_then_should_throw_an_exception__newSessionInstance(){
+    @DisplayName("Should throw an exception when some of the required fields are missing - newSessionInstance")
+    void when_some_of_the_required_fields_are_missing_then_should_throw_an_exception(){
         // given
+        setup(it -> {
+            given(it.findTopByOrderByCreationDateDesc()).willReturn(
+                Optional.of(newSessionStorage(LocalDate.now().minusDays(3)))
+            );
+        });
+
         given(this.restTemplateMock.execute(
             eq("/f?p=171"), eq(HttpMethod.GET), isNull(), any(ResponseExtractor.class)
         )).willReturn(Jsoup.parse("<input id=\"pInstance\" value=\"54321\"/><input id=\"pPageSubmissionId\" value=\"898900\"/>"));
@@ -130,17 +162,33 @@ public class ProductExternalServiceTest {
         verify(this.restTemplateMock, times(1)).execute(eq("/f?p=171"), eq(HttpMethod.GET), isNull(), any(ResponseExtractor.class));
         verify(this.restTemplateMock, only()).execute(eq("/f?p=171"), eq(HttpMethod.GET), isNull(), any(ResponseExtractor.class));
         verify(this.restTemplateMock, never()).postForEntity(anyString(), anyMap(), eq(String.class));
+        verify(this.sessionStorageRepository, times(1)).findTopByOrderByCreationDateDesc();
+        verify(this.sessionStorageRepository, never()).save(any(SessionStorage.class));
     }
 
     @Test
+    @DisplayName("Should throw an exception when ajaxIdentifier is not found - loginRequest")
     void when_ajaxIdentifier_is_not_found_then_should_throw_an_exception_loginRequest() {
         // given
+        setup(it -> {
+            given(it.findTopByOrderByCreationDateDesc()).willReturn(
+                Optional.of(newSessionStorage(LocalDate.now().minusDays(3)))
+            );
+        });
+
+        // Parse the login page
         given(this.restTemplateMock.execute(
-                eq("/f?p=171"), eq(HttpMethod.GET), isNull(), any(ResponseExtractor.class)
-        )).willReturn(Jsoup.parse(this.getGenericHtmlContent()));
+            eq("/f?p=171"), eq(HttpMethod.GET), isNull(), any(ResponseExtractor.class)
+        )).willReturn(Jsoup.parse(getGenericHtmlContent(this)));
 
-        given(this.restTemplateMock.postForEntity(eq("/wwv_flow.accept"), anyMap(), eq(String.class))).will(invocation -> null);
+        // Login into the system to get a cookie
+        given(this.restTemplateMock.postForEntity(eq("/wwv_flow.accept"), anyMap(), eq(String.class)))
+            .willReturn(ResponseEntity.ok()
+                .header("Set-Cookie", "COOKIE_SAVEG_MOBILE=ORA_WWV-dxVoldWhIfN2TviPcS9yhmcI")
+                .build()
+            );
 
+        // Parse page to get a Ajax Identifier
         given(
             this.restTemplateMock.execute(
                 eq("/f?p=171:2:54321:NEXT:NO:2:P2_CURSOR:B"),
@@ -161,11 +209,19 @@ public class ProductExternalServiceTest {
         verify(this.restTemplateMock, times(1)).execute(eq("/f?p=171"), eq(HttpMethod.GET), isNull(), any(ResponseExtractor.class));
         verify(this.restTemplateMock, times(1)).execute(eq("/f?p=171:2:54321:NEXT:NO:2:P2_CURSOR:B"), eq(HttpMethod.GET), isNull(), any(ResponseExtractor.class));
         verify(this.restTemplateMock, times(1)).postForEntity(eq("/wwv_flow.accept"), anyMap(), eq(String.class));
+        verify(this.sessionStorageRepository, times(1)).findTopByOrderByCreationDateDesc();
+        verify(this.sessionStorageRepository, never()).save(any(SessionStorage.class));
     }
 
     @Test
+    @DisplayName("Should return a projection - fetchByBarcode")
     void given_a_valid_barcode_then_should_return_a_projection_fetchByBarcode() throws JsonProcessingException {
         // given
+        setup(it -> {
+            given(it.findTopByOrderByCreationDateDesc())
+                .willReturn(Optional.of(newSessionStorage(LocalDate.now())));
+        });
+
         final String existingBarcode = "123456789101";
 
         final ProductWithLatestPrice expectedProjection = ProjectionFactory.productWithLatestPriceBuilder()
@@ -202,11 +258,19 @@ public class ProductExternalServiceTest {
         verify(this.restTemplateMock, times(1)).execute(eq("/wwv_flow.show"), eq(HttpMethod.POST), isNull(), any(ResponseExtractor.class));
         verify(this.restTemplateMock, times(1)).httpEntityCallback(any(HttpEntity.class), eq(String.class));
         verify(this.objectMapperMock, times(1)).readValue(anyString(), eq(ProductWithLatestPrice.class));
+        verify(this.sessionStorageRepository, times(1)).findTopByOrderByCreationDateDesc();
+        verify(this.sessionStorageRepository, never()).save(any(SessionStorage.class));
     }
 
     @Test
-    void given_a_non_existing_barcode_then_should_return_optional_empty_fetchByBarcode() throws JsonProcessingException {
+    @DisplayName("Should return an optional empty - fetchByBarcode ")
+    void given_a_non_existing_barcode_then_should_return_an_optional_empty_fetchByBarcode() throws JsonProcessingException {
         // given
+        setup(it -> {
+            given(it.findTopByOrderByCreationDateDesc())
+                .willReturn(Optional.of(newSessionStorage(LocalDate.now())));
+        });
+
         final String nonExistingBarcode = "1983471983474";
 
         given(this.restTemplateMock.httpEntityCallback(any(HttpEntity.class), eq(String.class))).willReturn(null);
@@ -240,11 +304,21 @@ public class ProductExternalServiceTest {
         verify(this.restTemplateMock, times(1)).execute(eq("/wwv_flow.show"), eq(HttpMethod.POST), isNull(), any(ResponseExtractor.class));
         verify(this.restTemplateMock, times(1)).httpEntityCallback(any(HttpEntity.class), eq(String.class));
         verify(this.objectMapperMock, times(1)).readValue(anyString(), eq(ProductWithLatestPrice.class));
+        verify(this.sessionStorageRepository, times(1)).findTopByOrderByCreationDateDesc();
+        verify(this.sessionStorageRepository, never()).save(any(SessionStorage.class));
+
+
     }
 
     @Test
+    @DisplayName("Should create a new session dynamically")
     void when_a_session_instance_is_not_valid_then_should_recreate_it() throws JsonProcessingException {
        //given
+        setup(it -> {
+            given(it.findTopByOrderByCreationDateDesc())
+                .willReturn(Optional.of(newSessionStorage(LocalDate.now().minusMonths(1))));
+        });
+
         final ProductWithLatestPrice expectedProjection = ProjectionFactory.productWithLatestPriceBuilder()
             .description("description")
             .barcode("123456789101")
@@ -257,7 +331,7 @@ public class ProductExternalServiceTest {
         given(this.restTemplateMock.httpEntityCallback(any(HttpEntity.class), eq(String.class))).willReturn(null);
 
         given(this.objectMapperMock.readValue(anyString(), eq(ProductWithLatestPrice.class)))
-            .willReturn(null)
+            .willThrow(InvalidDefinitionException.class)
             .willReturn(expectedProjection);
 
         given(this.restTemplateMock.execute(
@@ -290,6 +364,7 @@ public class ProductExternalServiceTest {
         verify(this.restTemplateMock, times(1)).execute(eq("/f?p=171"), eq(HttpMethod.GET), isNull(), any(ResponseExtractor.class));
         verify(this.restTemplateMock, times(1)).execute(eq("/f?p=171:2:54321:NEXT:NO:2:P2_CURSOR:B"), eq(HttpMethod.GET), isNull(), any(ResponseExtractor.class));
         verify(this.restTemplateMock, times(1)).postForEntity(eq("/wwv_flow.accept"), anyMap(), eq(String.class));
-
+        verify(this.sessionStorageRepository, times(1)).findTopByOrderByCreationDateDesc();
+        verify(this.sessionStorageRepository, times(1)).save(any(SessionStorage.class));
     }
 }
