7677df16be33662d60ccca20844de91103a97b2c
Neemias Santos <neemiassgc@outlook.com>
2022-07-24 15:00:30 +0100

    Create a helper class called ProductExternalServiceTestTools that will contain convenience methods for tests

---
+++ src/test/java/com/api/service/ProductExternalServiceTestTools.java
@@ -0,0 +1,11 @@
+package com.api.service;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+final class ProductExternalServiceTestTools {
+
+}
