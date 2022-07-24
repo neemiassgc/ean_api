be8ff50ad54324ba1e89e623b27a2122871347bc
Neemias Santos <neemiassgc@outlook.com>
2022-07-24 15:01:39 +0100

    Extract a convience method called getGenericHtmlContent from ProductExternalServiceTest ProductExternalServiceTestTools

--- src/test/java/com/api/service/ProductExternalServiceTestTools.java
+++ src/test/java/com/api/service/ProductExternalServiceTestTools.java
@@ -8,4 +8,16 @@ import java.util.stream.Collectors;
 
 final class ProductExternalServiceTestTools {
 
+    static <T> String getGenericHtmlContent(final T t) {
+        final InputStreamReader isr = new InputStreamReader(
+            Objects.requireNonNull(t.getClass().getClassLoader().getResourceAsStream("html_for_test.html"))
+        );
+
+        try (BufferedReader br = new BufferedReader(isr)) {
+            return br.lines().collect(Collectors.joining("\n"));
+        }
+        catch (IOException ignored) {
+            return "";
+        }
+    }
 }
