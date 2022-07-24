bba123fdf1bcec4193047ef16b31f5d71711f355
Neemias Santos <neemiassgc@outlook.com>
2022-07-24 15:29:42 +0100

    Add a convenience method called newSessionStorage to the ProductExternalServiceTestTools class

--- src/test/java/com/api/service/ProductExternalServiceTestTools.java
+++ src/test/java/com/api/service/ProductExternalServiceTestTools.java
@@ -1,8 +1,11 @@
 package com.api.service;
 
+import com.api.entity.SessionStorage;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.time.LocalDate;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
@@ -19,5 +22,15 @@ final class ProductExternalServiceTestTools {
         catch (IOException ignored) {
             return "";
         }
+    }
+
+    static SessionStorage newSessionStorage(final LocalDate localDate) {
+        return SessionStorage.builder()
+            .ajaxId("810EFA05686BAB7BE4DCFF431D456E4DF1DD4365A762A8F2AB07E15CD9902459")
+            .cookieKey("COOKIE_SAVEG_MOBILE")
+            .cookieValue("ORA_WWV-dxVoldWhIfN2TviPcS9yhmcI")
+            .creationDate(localDate)
+            .instance(23515127806187L)
+            .build();
     }
 }
