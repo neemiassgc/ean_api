fc8fa3cd4caa09f0492cfab22b1248c2230c17a9
Neemias Santos <neemiassgc@outlook.com>
2022-08-01 13:34:04 +0100

    Reflect type changes of test class ProductControllerTest that tests ProductController class

--- src/test/java/com/api/controller/ProductControllerTest.java
+++ src/test/java/com/api/controller/ProductControllerTest.java
@@ -1,6 +1,6 @@
 package com.api.controller;
 
-import com.api.service.PersistenceService;
+import com.api.repository.ProductRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -32,18 +32,18 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standal
 class ProductControllerTest {
 
     @MockBean
-    private PersistenceService persistenceService;
+    private ProductRepository productRepository;
 
     private MockMvc mockMvc;
 
     @BeforeEach
     void setUp() {
-        this.mockMvc = standaloneSetup(new ProductController(this.persistenceService)).alwaysDo(print()).build();
+        this.mockMvc = standaloneSetup(new ProductController(this.productRepository)).alwaysDo(print()).build();
     }
 
     @Test
     void when_GET_getAll_should_response_all_products_with_200() throws Exception {
-        given(persistenceService.<ProductWithManyPrices>findAllProducts()).willReturn(getProductList());
+        given(productRepository.<ProductWithManyPrices>findAllProducts()).willReturn(getProductList());
 
         mockMvc.perform(get("/api/products")
             .characterEncoding("UTF-8")
@@ -57,13 +57,13 @@ class ProductControllerTest {
         .andExpect(jsonPath("$[*].prices.*", hasSize(9)))
         .andExpect(jsonPath("$[0].prices[0].value").value(5.45));
 
-        verify(persistenceService, times(1)).findAllProducts();
-        verify(persistenceService, only()).findAllProducts();
+        verify(productRepository, times(1)).findAllProducts();
+        verify(productRepository, only()).findAllProducts();
     }
 
     @Test
     void when_GET_getAll_should_response_a_empty_list_with_200() throws Exception {
-        given(persistenceService.findAllProducts()).willReturn(Collections.emptyList());
+        given(productRepository.findAllProducts()).willReturn(Collections.emptyList());
 
         mockMvc.perform(get("/api/products")
             .characterEncoding("UTF-8")
@@ -73,13 +73,13 @@ class ProductControllerTest {
         .andExpect(content().contentType(MediaType.APPLICATION_JSON))
         .andExpect(jsonPath("$").isEmpty());
 
-        verify(persistenceService, times(1)).findAllProducts();
-        verify(persistenceService, only()).findAllProducts();
+        verify(productRepository, times(1)).findAllProducts();
+        verify(productRepository, only()).findAllProducts();
     }
 
     @Test
     void when_GET_getAll_should_response_a_paged_product_list_with_200() throws Exception {
-        given(persistenceService.findAllPagedProducts(Mockito.any(Pageable.class))).willReturn(getPagedProductList());
+        given(productRepository.findAllPagedProducts(Mockito.any(Pageable.class))).willReturn(getPagedProductList());
 
         mockMvc.perform(get("/api/products?pag=0-3")
             .characterEncoding("UTF-8")
@@ -97,14 +97,14 @@ class ProductControllerTest {
         .andExpect(jsonPath("$.hasNext").value(false))
         .andExpect(jsonPath("$.currentPage").value(0));
 
-        verify(persistenceService, never()).findAllProducts();
+        verify(productRepository, never()).findAllProducts();
     }
 
     @Test
     void when_GET_getByBarcode_should_response_a_product_with_all_prices_with_200() throws Exception {
         final String validBarcode = "7891962057620";
 
-        given(persistenceService.findProductByBarcode(eq(validBarcode), eq(0))).willReturn(getNewProduct("5.45", "8.56", "6.3"));
+        given(productRepository.findProductByBarcode(eq(validBarcode), eq(0))).willReturn(getNewProduct("5.45", "8.56", "6.3"));
 
         mockMvc.perform(get("/api/products/"+validBarcode)
             .accept(MediaType.APPLICATION_JSON)
@@ -119,8 +119,8 @@ class ProductControllerTest {
         .andExpect(jsonPath("$.prices", hasSize(3)))
         .andExpect(jsonPath("$.prices[*].value", contains(5.45, 8.56, 6.3)));
 
-        verify(persistenceService, times(1)).findProductByBarcode(eq(validBarcode), eq(0));
-        verify(persistenceService, only()).findProductByBarcode(eq(validBarcode), eq(0));
+        verify(productRepository, times(1)).findProductByBarcode(eq(validBarcode), eq(0));
+        verify(productRepository, only()).findProductByBarcode(eq(validBarcode), eq(0));
     }
 
     @Test
@@ -128,7 +128,7 @@ class ProductControllerTest {
         final String validBarcode = "7891962057620";
         final int limitOfPrices = 1;
 
-        given(persistenceService.findProductByBarcode(eq(validBarcode), eq(limitOfPrices)))
+        given(productRepository.findProductByBarcode(eq(validBarcode), eq(limitOfPrices)))
             .willReturn(getNewProduct());
 
         mockMvc.perform(get("/api/products/"+validBarcode+"?lop="+limitOfPrices)
@@ -144,8 +144,8 @@ class ProductControllerTest {
         .andExpect(jsonPath("$.prices").doesNotExist())
         .andExpect(jsonPath("$.latestPrice.value", is(2.6)));
 
-        verify(persistenceService, times(1)).findProductByBarcode(eq(validBarcode), eq(limitOfPrices));
-        verify(persistenceService, only()).findProductByBarcode(eq(validBarcode), eq(limitOfPrices));
+        verify(productRepository, times(1)).findProductByBarcode(eq(validBarcode), eq(limitOfPrices));
+        verify(productRepository, only()).findProductByBarcode(eq(validBarcode), eq(limitOfPrices));
     }
 
     @Test
@@ -153,7 +153,7 @@ class ProductControllerTest {
         final String validBarcode = "7891962057620";
         final int limitOfPrices = 2;
 
-        given(persistenceService.findProductByBarcode(eq(validBarcode), eq(limitOfPrices))).willReturn(getNewProduct("2.6", "3.4"));
+        given(productRepository.findProductByBarcode(eq(validBarcode), eq(limitOfPrices))).willReturn(getNewProduct("2.6", "3.4"));
 
         mockMvc.perform(get("/api/products/"+validBarcode+"?lop="+limitOfPrices)
             .accept(MediaType.APPLICATION_JSON)
@@ -168,7 +168,7 @@ class ProductControllerTest {
         .andExpect(jsonPath("$.prices", hasSize(2)))
         .andExpect(jsonPath("$.prices[*].value", contains(2.6, 3.4)));
 
-        verify(persistenceService, times(1)).findProductByBarcode(eq(validBarcode), eq(limitOfPrices));
-        verify(persistenceService, only()).findProductByBarcode(eq(validBarcode), eq(limitOfPrices));
+        verify(productRepository, times(1)).findProductByBarcode(eq(validBarcode), eq(limitOfPrices));
+        verify(productRepository, only()).findProductByBarcode(eq(validBarcode), eq(limitOfPrices));
     }
 }
