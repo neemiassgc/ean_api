4fc48d81b555fa09d1a9e3847aae811474f7e93f
Neemias Santos <neemiassgc@outlook.com>
2022-08-01 18:28:24 +0100

    Switch to FetchType.LAZY on Price#Product

--- src/main/java/com/api/entity/Price.java
+++ src/main/java/com/api/entity/Price.java
@@ -24,7 +24,7 @@ public class Price {
     @Column(name = "instant", columnDefinition = "TIMESTAMPTZ")
     private Instant instant = Instant.now();
 
-    @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+    @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.PERSIST}, fetch = FetchType.LAZY)
     @JoinColumn(foreignKey = @ForeignKey(name = "fk_product_prices"))
     private Product product;
 
