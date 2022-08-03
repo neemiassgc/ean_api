41b68e64644e3e055f08e95abf26aa943da050ec
Neemias Santos <neemiassgc@outlook.com>
2022-08-03 23:48:39 +0100

    Extract methods handleMethodArgumentNotValid, handleConstraintViolationException and handleResponseStatusException from the ProductController to the GlobalErrorHandlingController

--- src/main/java/com/api/controller/GlobalErrorHandlingController.java
+++ src/main/java/com/api/controller/GlobalErrorHandlingController.java
@@ -1,7 +1,50 @@
 package com.api.controller;
 
+import com.api.error.ErrorTemplate;
+import com.api.error.Violation;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.validation.ConstraintViolationException;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalErrorHandlingController {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ErrorTemplate handleMethodArgumentNotValid(final MethodArgumentNotValidException mnv) {
+        final Set<Violation> violations = mnv.getBindingResult()
+            .getFieldErrors()
+            .stream()
+            .map(fieldError -> new Violation(fieldError.getField(), fieldError.getDefaultMessage()))
+            .collect(Collectors.toSet());
+
+        return new ErrorTemplate(violations);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ErrorTemplate handleConstraintViolationException(final ConstraintViolationException cve) {
+        final Set<Violation> violations = cve.getConstraintViolations()
+            .stream()
+            .map(constraint -> new Violation(constraint.getInvalidValue().toString(), constraint.getMessage()))
+            .collect(Collectors.toSet());
+
+        return new ErrorTemplate(violations);
+    }
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<String> handleResponseStatusException(final ResponseStatusException rse) {
+        return ResponseEntity
+            .status(rse.getStatus())
+            .body(Objects.requireNonNullElseGet(rse.getReason(), () -> "No reasons"));
+    }
 }
--- src/main/java/com/api/controller/ProductController.java
+++ src/main/java/com/api/controller/ProductController.java
@@ -2,27 +2,19 @@ package com.api.controller;
 
 import com.api.annotation.Barcode;
 import com.api.annotation.Number;
-import com.api.error.ErrorTemplate;
-import com.api.error.Violation;
 import com.api.pojo.DomainUtils;
 import com.api.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
-import javax.validation.ConstraintViolationException;
 import javax.validation.constraints.Pattern;
 import java.util.Objects;
-import java.util.Set;
-import java.util.stream.Collectors;
 
-import static com.api.projection.Projection.*;
+import static com.api.projection.Projection.ProductBase;
 
 @RestController
 @RequestMapping(path = "/api")
@@ -49,35 +41,5 @@ public class ProductController {
         @Number(message = "lop must be a positive number or zero") String limitOfPrices
     ) {
         return productRepository.findProductByBarcode(barcode, Integer.parseInt(limitOfPrices));
-    }
-
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ErrorTemplate handleMethodArgumentNotValid(final MethodArgumentNotValidException mnv) {
-        final Set<Violation> violations = mnv.getBindingResult()
-            .getFieldErrors()
-            .stream()
-            .map(fieldError -> new Violation(fieldError.getField(), fieldError.getDefaultMessage()))
-            .collect(Collectors.toSet());
-
-        return new ErrorTemplate(violations);
-    }
-
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(ConstraintViolationException.class)
-    public ErrorTemplate handleConstraintViolationException(final ConstraintViolationException cve) {
-        final Set<Violation> violations = cve.getConstraintViolations()
-            .stream()
-            .map(constraint -> new Violation(constraint.getInvalidValue().toString(), constraint.getMessage()))
-            .collect(Collectors.toSet());
-
-        return new ErrorTemplate(violations);
-    }
-
-    @ExceptionHandler(ResponseStatusException.class)
-    public ResponseEntity<String> handleResponseStatusException(final ResponseStatusException rse) {
-        return ResponseEntity
-            .status(rse.getStatus())
-            .body(Objects.requireNonNullElseGet(rse.getReason(), () -> "No reasons"));
     }
 }
