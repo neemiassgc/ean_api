030c87c7d7385021e3b1eeff8fba3da7e3b34fed
Neemias Santos <neemiassgc@outlook.com>
2022-08-03 23:34:31 +0100

    Add an end-point GET /api/prices?barcode= to the PriceController

--- src/main/java/com/api/controller/PriceController.java
+++ src/main/java/com/api/controller/PriceController.java
@@ -1,21 +1,20 @@
 package com.api.controller;
 
+import com.api.annotation.Barcode;
 import com.api.entity.Price;
+import static com.api.projection.Projection.*;
 import com.api.repository.PriceRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.List;
 import java.util.UUID;
-
-import static com.api.projection.Projection.PriceWithInstant;
+import java.util.stream.Collectors;
 
 @Validated
 @RestController
@@ -31,5 +30,16 @@ public class PriceController {
             .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Price not found"));
 
         return new PriceWithInstant(fetchedPrice.getValue(), fetchedPrice.getInstant());
+    }
+
+    @GetMapping(path = "/prices", params = "barcode", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<PriceWithInstant> searchByProductBarcode(@RequestParam("barcode") @Barcode final String barcode) {
+        final List<Price> priceList = priceRepository.findByProductBarcodeOrderByInstantDesc(barcode);
+
+        if (priceList.isEmpty()) throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found");
+
+        return priceList.stream()
+            .map(price -> new PriceWithInstant(price.getValue(), price.getInstant()))
+            .collect(Collectors.toList());
     }
 }
