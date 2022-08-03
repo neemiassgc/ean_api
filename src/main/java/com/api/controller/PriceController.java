e81590964edfab59f34e1bdc4de8ca04526767ab
Neemias Santos <neemiassgc@outlook.com>
2022-08-03 23:33:56 +0100

    Add an end-point GET /api/prices/{id} to the PriceController

--- src/main/java/com/api/controller/PriceController.java
+++ src/main/java/com/api/controller/PriceController.java
@@ -1,11 +1,21 @@
 package com.api.controller;
 
+import com.api.entity.Price;
 import com.api.repository.PriceRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.UUID;
+
+import static com.api.projection.Projection.PriceWithInstant;
 
 @Validated
 @RestController
@@ -14,4 +24,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class PriceController {
 
     private final PriceRepository priceRepository;
+
+    @GetMapping(path = "/prices/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public PriceWithInstant searchById(@PathVariable("id") final UUID id) {
+        final Price fetchedPrice = priceRepository.findById(id)
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Price not found"));
+
+        return new PriceWithInstant(fetchedPrice.getValue(), fetchedPrice.getInstant());
+    }
 }
