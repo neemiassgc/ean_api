963774e233a282844119f44beed866fe744bb2b7
Neemias Santos <neemiassgc@outlook.com>
2022-08-05 00:20:09 +0100

    Add to the DomainMapper service a new method called mapToSimpleProduct

--- src/main/java/com/api/service/DomainMapper.java
+++ src/main/java/com/api/service/DomainMapper.java
@@ -1,7 +1,30 @@
 package com.api.service;
 
+import static com.api.projection.Projection.*;
+
+import com.api.entity.Product;
+import lombok.NonNull;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DomainMapper {
+
+    public SimpleProduct mapToSimpleProduct(@NonNull final Product product) {
+        return new SimpleProduct() {
+            @Override
+            public String getDescription() {
+                return product.getDescription();
+            }
+
+            @Override
+            public String getBarcode() {
+                return product.getBarcode();
+            }
+
+            @Override
+            public Integer getSequenceCode() {
+                return product.getSequenceCode();
+            }
+        };
+    }
 }
