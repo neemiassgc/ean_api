99f3f66330f01c4e6a406a9ff4ef18b12a5ced63
Neemias Santos <neemiassgc@outlook.com>
2022-08-05 00:10:04 +0100

    Refactor ProductRepositoryCustom interface and its implementation ProductRepositoryCustomImpl to make them keep only findProductByBarcode method

--- src/main/java/com/api/repository/ProductRepositoryCustom.java
+++ src/main/java/com/api/repository/ProductRepositoryCustom.java
@@ -1,20 +1,9 @@
 package com.api.repository;
 
-import com.api.projection.Projection;
+import com.api.entity.Product;
 import lombok.NonNull;
-import org.springframework.data.domain.Pageable;
-
-import java.util.List;
 
 public interface ProductRepositoryCustom {
 
-    <I extends Projection.ProductBase> I findProductByBarcode(@NonNull final String barcode, int limit);
-
-    List<Projection.ProductWithManyPrices> findAllProducts();
-
-    Projection.Paged<List<Projection.ProductWithManyPrices>> findAllPagedProducts(@NonNull final Pageable pageable);
-
-    List<Projection.ProductWithLatestPrice> findAllProductsWithLatestPrice();
-
-    void saveProductWithPrice(@NonNull final Projection.ProductWithLatestPrice productWithLatestPrice);
+    Product processByBarcode(@NonNull final String barcode);
 }
--- src/main/java/com/api/repository/ProductRepositoryCustomImpl.java
+++ src/main/java/com/api/repository/ProductRepositoryCustomImpl.java
@@ -1,25 +1,17 @@
 package com.api.repository;
 
-import com.api.entity.Price;
-import com.api.projection.ProjectionFactory;
-import com.api.service.DomainMapper;
+import com.api.entity.Product;
 import com.api.service.ProductExternalService;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.List;
-import java.util.UUID;
-
-import static com.api.projection.Projection.*;
+import java.util.Optional;
 
 @SuppressWarnings("unchecked")
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
@@ -29,50 +21,17 @@ public class ProductRepositoryCustomImpl implements ProductRepositoryCustom {
     @Setter(onMethod_ = @Autowired, onParam_ = @Lazy)
     private ProductRepository productRepository;
 
-    private final PriceRepository priceRepository;
     private final ProductExternalService productExternalService;
-    private final DomainMapper domainMapper;
 
-    public <I extends ProductBase> I findProductByBarcode(@NonNull final String barcode, int limit) {
-        if (limit < 0) throw new IllegalArgumentException("limit must be a positive number or zero");
+    public Product processByBarcode(@NonNull final String barcode) {
+        final Optional<Product> productOptional = productRepository.findByBarcode(barcode);
 
-        final List<Price> priceList =
-        priceRepository.findAllByProductBarcode(
-            barcode,
-            PageRequest.ofSize(limit == 0 ? Integer.MAX_VALUE : limit)
-        );
-
-        if (!priceList.isEmpty())
-            return (I) domainMapper.toProductWithManyPrices(priceList);
+        if (productOptional.isPresent()) return productOptional.get();
 
         // Save a new product
-        final ProductBase productBase = productExternalService.fetchByBarcode(barcode)
+        final Product newProduct = productExternalService.fetchByBarcode(barcode)
             .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found"));
 
-        this.saveProductWithPrice((ProductWithLatestPrice) productBase);
-
-        return (I) productBase;
-    }
-
-    public List<ProductWithManyPrices> findAllProducts() {
-        final List<Price> priceList = priceRepository.findAll();
-        return domainMapper.toProductListWithManyPrices(priceList);
-    }
-
-    public Paged<List<ProductWithManyPrices>> findAllPagedProducts(@NonNull final Pageable pageable) {
-        final Page<UUID> page = productRepository.findAllId(pageable);
-        final List<ProductWithManyPrices> productWithManyPricesList =
-            domainMapper.toProductListWithManyPrices(priceRepository.findAllByProductId(page.getContent()));
-
-        return ProjectionFactory.paged(page, productWithManyPricesList);
-    }
-
-    public List<ProductWithLatestPrice> findAllProductsWithLatestPrice() {
-        return domainMapper.toProductListWithLatestPrice(priceRepository.findAllLatestPrice());
-    }
-
-    public void saveProductWithPrice(@NonNull final ProductWithLatestPrice productWithLatestPrice) {
-        final Price priceToSave = domainMapper.mapToPrice(productWithLatestPrice);
-        priceRepository.save(priceToSave);
+        return this.productRepository.save(newProduct);
     }
 }
