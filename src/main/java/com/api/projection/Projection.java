3fb16fbfb2afcc9b9349c7355a51a8847cc5ed33
Neemias Santos <neemiassgc@outlook.com>
2022-08-05 00:14:20 +0100

    Remove projections ProductWithLatestPrice, ProductWithManyPrices and rename projection ProductBase to SimpleProduct

--- src/main/java/com/api/projection/Projection.java
+++ src/main/java/com/api/projection/Projection.java
@@ -1,35 +1,20 @@
 package com.api.projection;
 
-import com.api.projection.deserializer.ProductWithLatestPriceDeserializer;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
-import org.springframework.beans.factory.annotation.Value;
 
 import java.math.BigDecimal;
 import java.time.Instant;
-import java.util.List;
 
 public final class Projection {
 
     private Projection() {}
 
-    public interface ProductBase {
+    public interface SimpleProduct {
         String getDescription();
         String getBarcode();
         Integer getSequenceCode();
-    }
-
-    @JsonDeserialize(using = ProductWithLatestPriceDeserializer.class)
-    public interface ProductWithLatestPrice extends ProductBase {
-
-        @Value("#{new com.api.projection.Projection.PriceWithInstant(new java.math.BigDecimal(target.latestPrice.toString()), target.instant)}")
-        PriceWithInstant getLatestPrice();
-    }
-
-    public interface ProductWithManyPrices extends ProductBase {
-        List<PriceWithInstant> getPrices();
     }
 
     public interface Paged<T> {
