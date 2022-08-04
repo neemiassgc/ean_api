bfa15dbf1b51216ab65efb916dd1722fe8695a17
Neemias Santos <neemiassgc@outlook.com>
2022-08-05 00:12:23 +0100

    Remove ProductRepository's findAllId method and optimize imports

--- src/main/java/com/api/repository/ProductRepository.java
+++ src/main/java/com/api/repository/ProductRepository.java
@@ -1,17 +1,11 @@
 package com.api.repository;
 
 import com.api.entity.Product;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, UUID>, ProductRepositoryCustom {
-
-    @Query("SELECT p.id FROM Product p ORDER BY p.description asc")
-    Page<UUID> findAllId(Pageable pageable);
 }
