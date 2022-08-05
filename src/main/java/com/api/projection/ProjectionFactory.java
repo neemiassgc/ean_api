37ff56fa9b0a5d0a5b9e4560432094c1e6e0ab0b
Neemias Santos <neemiassgc@outlook.com>
2022-08-05 01:27:06 +0100

    Keep only the ProjectionFactory's method paged and delete all other

--- src/main/java/com/api/projection/ProjectionFactory.java
+++ src/main/java/com/api/projection/ProjectionFactory.java
@@ -1,72 +1,10 @@
 package com.api.projection;
 
-import lombok.Builder;
 import org.springframework.data.domain.Page;
 
-import java.util.List;
-
-import static com.api.projection.Projection.*;
+import static com.api.projection.Projection.Paged;
 
 public class ProjectionFactory {
-    @Builder(builderMethodName = "productWithManyPricesBuilder")
-    public static ProductWithManyPrices productWithManyPrices(
-            final String description,
-            final String barcode,
-            final Integer sequenceCode,
-            final List<PriceWithInstant> prices
-    ) {
-        return new ProductWithManyPrices() {
-            @Override
-            public String getDescription() {
-                return description;
-            }
-
-            @Override
-            public String getBarcode() {
-                return barcode;
-            }
-
-            @Override
-            public Integer getSequenceCode() {
-                return sequenceCode;
-            }
-
-            @Override
-            public List<PriceWithInstant> getPrices() {
-                return prices;
-            }
-        };
-    }
-
-    @Builder(builderMethodName = "productWithLatestPriceBuilder")
-    public static ProductWithLatestPrice productWithLatestPrice(
-            final String description,
-            final String barcode,
-            final Integer sequenceCode,
-            final PriceWithInstant latestPrice
-    ) {
-        return new ProductWithLatestPrice() {
-            @Override
-            public String getDescription() {
-                return description;
-            }
-
-            @Override
-            public String getBarcode() {
-                return barcode;
-            }
-
-            @Override
-            public Integer getSequenceCode() {
-                return sequenceCode;
-            }
-
-            @Override
-            public PriceWithInstant getLatestPrice() {
-                return latestPrice;
-            }
-        };
-    }
 
     public static <T> Paged<T> paged(Page<?> page, final T content) {
         return new Paged<T>() {
