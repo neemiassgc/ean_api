d8c599b3133639ce635f6c4a089d3268accb993a
Neemias Santos <neemiassgc@outlook.com>
2022-08-05 20:27:22 +0100

    Add a JPQL query method called findAllWithLastPrice to the ProductRespository class

--- src/main/java/com/api/repository/ProductRepository.java
+++ src/main/java/com/api/repository/ProductRepository.java
@@ -2,10 +2,22 @@ package com.api.repository;
 
 import com.api.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, UUID>, ProductRepositoryCustom {
+
+    Optional<Product> findByBarcode(String barcode);
+
+    @Query(
+        "SELECT pro FROM Product pro JOIN FETCH pro.prices pri WHERE pri.instant IN "+
+        "(SELECT MAX(pri2.instant) FROM Price pri2 GROUP BY pri2.product) "+
+        "ORDER BY pro.description ASC"
+    )
+    List<Product> findAllWithLastPrice();
 }
