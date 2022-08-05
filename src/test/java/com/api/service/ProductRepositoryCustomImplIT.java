b18be3805fa66aae371ad3b914303d95ac98c35e
Neemias Santos <neemiassgc@outlook.com>
2022-08-06 00:08:20 +0100

    Add a test method called should_return_a_product_from_the_external_service to the ProductRepositoryCustomImplIT

--- src/test/java/com/api/service/ProductRepositoryCustomImplIT.java
+++ src/test/java/com/api/service/ProductRepositoryCustomImplIT.java
@@ -38,4 +38,19 @@ public class ProductRepositoryCustomImplIT {
         assertThat(product.getBarcode()).isEqualTo("7897534852624");
         assertThat(product.getId()).isEqualTo(UUID.fromString("7e49cbbf-0d4b-4a67-b108-346bef1c961f"));
     }
+
+    @Test
+    @Transactional
+    public void should_return_a_product_from_the_external_service() {
+        final String targetBarcode = "7892840819507";
+        final Product product =
+            productRepositoryCustomImplUnderTest.processByBarcode(targetBarcode);
+
+        assertThat(product).isNotNull();
+        assertThat(product.getDescription()).isEqualTo("ACHOC PO TODDY 370G");
+        assertThat(product.getBarcode()).isEqualTo("7892840819507");
+        assertThat(product.getId()).isNotNull();
+        assertThat(productRepository.count()).isEqualTo(12);
+        assertThat(priceRepository.count()).isEqualTo(67);
+    }
 }
