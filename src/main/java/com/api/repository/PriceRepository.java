e25e3eda73a288eecb11cdca8fa5a0cfbd4cfe06
Neemias Santos <neemiassgc@outlook.com>
2022-08-05 20:29:46 +0100

    Delete all PriceRepository's no longer required custom methods and keep only findByProductBarcodeOrderByInstantDesc

--- src/main/java/com/api/repository/PriceRepository.java
+++ src/main/java/com/api/repository/PriceRepository.java
@@ -1,10 +1,7 @@
 package com.api.repository;
 
 import com.api.entity.Price;
-import lombok.NonNull;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,17 +10,5 @@ import java.util.UUID;
 @Repository
 public interface PriceRepository extends JpaRepository<Price, UUID> {
 
-    @Query("SELECT p FROM Price p JOIN FETCH p.product WHERE p.instant IN (SELECT MAX(pr.instant) FROM Price pr GROUP BY pr.product)")
-    List<Price> findAllLatestPrice();
-
-    @Query("SELECT p FROM Price p JOIN FETCH p.product WHERE p.product.barcode = ?1 ORDER BY p.instant DESC")
-    List<Price> findAllByProductBarcode(@NonNull String barcode, Pageable pageable);
-
-    @Query("SELECT p FROM Price p JOIN FETCH p.product WHERE p.product.id IN ?1")
-    List<Price> findAllByProductId(@NonNull List<UUID> uuids);
-
-    @Override
-    @NonNull
-    @Query("SELECT p FROM Price p JOIN FETCH p.product")
-    List<Price> findAll();
+    List<Price> findByProductBarcodeOrderByInstantDesc(String barcode);
 }
