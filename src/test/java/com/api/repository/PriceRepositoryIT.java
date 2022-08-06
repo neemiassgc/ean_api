d323c80bf4898a4c603df1f595a78ce2ddd320bb
Neemias Santos <neemiassgc@outlook.com>
2022-08-06 04:12:42 +0100

    Add a test method called should_return_a_list_of_prices_of_a_product to the PriceRepositoryIT

--- src/test/java/com/api/repository/PriceRepositoryIT.java
+++ src/test/java/com/api/repository/PriceRepositoryIT.java
@@ -1,8 +1,16 @@
 package com.api.repository;
 
+import com.api.entity.Price;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.util.Iterator;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional(readOnly = true)
@@ -10,4 +18,20 @@ public class PriceRepositoryIT {
 
     @Autowired
     private PriceRepository priceRepository;
+
+    @Test
+    void should_return_a_list_of_prices_of_a_product() {
+        final String barcode = "7896036093085";
+        final List<Price> priceList = priceRepository.findByProductBarcodeOrderByInstantDesc(barcode);
+        final Iterator<BigDecimal> valuesToCheckInOrder = List.of(
+            new BigDecimal("3.50"),
+            new BigDecimal("2.50"),
+            new BigDecimal("5.49")
+        ).iterator();
+
+        assertThat(priceList).hasSize(3);
+        assertThat(priceList).allSatisfy(priceUnderTest ->
+            assertThat(priceUnderTest.getValue()).isEqualTo(valuesToCheckInOrder.next())
+        );
+    }
 }
