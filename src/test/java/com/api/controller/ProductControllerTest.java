ce7989395f5027e24fc14b6e7c9699c5c44f2cd4
Neemias Santos <neemiassgc@outlook.com>
2022-08-06 18:01:14 +0100

    Add a new test method named when_GET_getAll_should_response_all_products_with_200 to the ProductControllerTest test class

--- src/test/java/com/api/controller/ProductControllerTest.java
+++ src/test/java/com/api/controller/ProductControllerTest.java
@@ -5,14 +5,24 @@ import com.api.projection.Projection;
 import com.api.repository.ProductRepository;
 import com.api.service.DomainMapper;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 @WebMvcTest(ProductController.class)
@@ -74,5 +84,33 @@ class ProductControllerTest {
     @BeforeEach
     void setUp() {
         this.mockMvc = standaloneSetup(new ProductController(productRepository, domainMapper)).alwaysDo(print()).build();
+    }
+
+    @Test
+    void when_GET_getAll_should_response_all_products_with_200() throws Exception {
+        final String urlBase = "http://localhost/api/prices?barcode=";
+        given(productRepository.findAll()).willReturn(Resources.products);
+        given(domainMapper.mapToSimpleProductList(eq(Resources.products))).willReturn(Resources.simpleProducts);
+
+        mockMvc.perform(get("/api/products")
+            .characterEncoding("UTF-8")
+            .accept(MediaType.APPLICATION_JSON)
+        )
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$").isArray())
+        .andExpect(jsonPath("$", hasSize(3)))
+        .andExpect(
+            jsonPath("$[*].barcode", contains("7891000055120", "7896336010058", "7896656800018"))
+        )
+        .andExpect(
+            jsonPath(
+                "$[*].links[0].href",
+                contains(urlBase+"7891000055120", urlBase+"7896336010058", urlBase+"7896656800018")
+            )
+        );
+
+        verify(productRepository, times(1)).findAll();
+        verify(domainMapper, times(1)).mapToSimpleProductList(eq(Resources.products));
     }
 }
