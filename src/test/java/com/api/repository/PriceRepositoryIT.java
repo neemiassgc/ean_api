e4b292f5d2aaa4af55afaff12d76d5f0095542ef
Neemias Santos <neemiassgc@outlook.com>
2022-08-08 00:29:49 +0100

    Add a  test method called should_return_only_two_prices_of_a_product to the PriceRepositoryIT

--- src/test/java/com/api/repository/PriceRepositoryIT.java
+++ src/test/java/com/api/repository/PriceRepositoryIT.java
@@ -4,6 +4,7 @@ import com.api.entity.Price;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -31,6 +32,22 @@ public class PriceRepositoryIT {
         ).iterator();
 
         assertThat(priceList).hasSize(3);
+        assertThat(priceList).allSatisfy(priceUnderTest ->
+            assertThat(priceUnderTest.getValue()).isEqualTo(valuesToCheckInOrder.next())
+        );
+    }
+
+    @Test
+    void should_return_only_two_prices_of_a_product() {
+        final String barcode = "7896036093085";
+        final List<Price> priceList =
+            priceRepository.findByProductBarcode(barcode, PageRequest.ofSize(2).withSort(Sort.by("instant").descending()));
+        final Iterator<BigDecimal> valuesToCheckInOrder = List.of(
+                new BigDecimal("3.50"),
+                new BigDecimal("2.50")
+        ).iterator();
+
+        assertThat(priceList).hasSize(2);
         assertThat(priceList).allSatisfy(priceUnderTest ->
             assertThat(priceUnderTest.getValue()).isEqualTo(valuesToCheckInOrder.next())
         );
