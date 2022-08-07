015fee44bacd9077158e51a57b43fe7e618c53bf
Neemias Santos <neemiassgc@outlook.com>
2022-08-07 21:57:13 +0100

    Refactor test class ProductControllerIT so that it comforms to the changes

--- src/test/java/com/api/controller/ProductControllerIT.java
+++ src/test/java/com/api/controller/ProductControllerIT.java
@@ -1,5 +1,7 @@
 package com.api.controller;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -23,150 +25,232 @@ public class ProductControllerIT {
     
     private static final String DEFAULT_URL = "/api/products";
 
-    @Test
-    void when_GET_getAll_should_response_all_products_with_200() throws Exception {
-        mockMvc.perform(get(DEFAULT_URL)
-            .characterEncoding(StandardCharsets.UTF_8)
-            .contentType(MediaType.APPLICATION_JSON)
-        )
-        .andExpect(status().isOk())
-        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-        .andExpect(jsonPath("$").isArray())
-        .andExpect(jsonPath("$", hasSize(11)))
-        .andExpect(jsonPath("$[0].description").value("ACHOC PO NESCAU 800G"))
-        .andExpect(jsonPath("$[0].sequenceCode").value(is(29250)))
-        .andExpect(jsonPath("$[0].barcode").value("7891000055120"))
-        .andExpect(jsonPath("$[*].prices[*].value", hasSize(66)))
-        .andExpect(jsonPath("$[0].prices[*].value", contains(12.7, 19.0, 16.5, 6.61, 16.8, 9.85, 10.6, 16.1, 12.6, 19.1)))
-        .andExpect(jsonPath("$[1].prices[*].value", contains(5.65, 9.9, 10.75, 7.5)));
+
+    @Nested
+    class GetAllTest {
+
+        @Test
+        @DisplayName("GET "+DEFAULT_URL+" - 200 OK")
+        void should_return_all_products_with_200() throws Exception {
+            mockMvc.perform(get(DEFAULT_URL)
+                .characterEncoding(StandardCharsets.UTF_8)
+                .contentType(MediaType.APPLICATION_JSON)
+            )
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$").isArray())
+            .andExpect(jsonPath("$", hasSize(11)))
+            // Verifying the first product
+            .andExpect(jsonPath("$[0].description").value("ACHOC PO NESCAU 800G"))
+            .andExpect(jsonPath("$[0].sequenceCode").value(is(29250)))
+            .andExpect(jsonPath("$[0].barcode").value("7891000055120"))
+            .andExpect(jsonPath("$[0].links[0].rel").value("prices"))
+            .andExpect(jsonPath("$[0].links[0].href").value("http://localhost/api/prices?barcode=7891000055120"))
+            .andExpect(jsonPath("$[0].links[1].rel").value("self"))
+            .andExpect(jsonPath("$[0].links[1].href").value("http://localhost/api/products/7891000055120"))
+            // Verifying the last product
+            .andExpect(jsonPath("$[10].description").value("PAO BAUDUC 400G INTE"))
+            .andExpect(jsonPath("$[10].sequenceCode").value(is(134262)))
+            .andExpect(jsonPath("$[10].barcode").value("7891962057620"))
+            .andExpect(jsonPath("$[10].links[0].rel").value("prices"))
+            .andExpect(jsonPath("$[10].links[0].href").value("http://localhost/api/prices?barcode=7891962057620"))
+            .andExpect(jsonPath("$[10].links[1].rel").value("self"))
+            .andExpect(jsonPath("$[10].links[1].href").value("http://localhost/api/products/7891962057620"))
+            // Verifying the order
+            .andExpect(jsonPath(
+                "$[*].sequenceCode",
+                contains(29250, 137513, 120983, 93556, 142862, 113249, 2909, 105711, 9785, 1184, 134262)
+            ));
+        }
+
+        @Test
+        @DisplayName("GET "+DEFAULT_URL+"?pag=0-5 - 200 OK")
+        void should_return_the_first_page_of_paged_products_with_200() throws Exception {
+            final String firstPage = "0-5";
+            final String urlBasePrices = "http://localhost/api/prices?barcode=";
+            final String urlBaseSelf = "http://localhost/api/products/";
+
+            mockMvc.perform(get(DEFAULT_URL).queryParam("pag", firstPage)
+                .characterEncoding(StandardCharsets.UTF_8)
+                .contentType(MediaType.APPLICATION_JSON)
+            )
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$.content").isArray())
+            .andExpect(jsonPath("$.content", hasSize(5)))
+            .andExpect(jsonPath("$.currentPage").value(0))
+            .andExpect(jsonPath("$.totalPages").value(3))
+            .andExpect(jsonPath("$.numberOfItems").value(5))
+            .andExpect(jsonPath("$.hasNext").value(true))
+            .andExpect(
+                jsonPath("$.content[*].barcode",
+                contains("7891000055120", "7897534852624", "7896336010058", "7898279792299", "7896045104482"))
+            )
+            .andExpect(jsonPath("$.content[*].links[0].rel", everyItem(is("prices"))))
+            .andExpect(jsonPath("$.content[*].links[1].rel", everyItem(is("self"))))
+            .andExpect(jsonPath(
+                "$.content[*].links[0].href",
+                contains(
+                    urlBasePrices+"7891000055120", urlBasePrices+"7897534852624",
+                    urlBasePrices+"7896336010058", urlBasePrices+"7898279792299", urlBasePrices+"7896045104482"
+                )
+            ))
+            .andExpect(jsonPath(
+                "$.content[*].links[1].href",
+                contains(
+                    urlBaseSelf+"7891000055120", urlBaseSelf+"7897534852624",
+                    urlBaseSelf+"7896336010058", urlBaseSelf+"7898279792299", urlBaseSelf+"7896045104482"
+                )
+            ))
+            .andExpect(jsonPath("$.links[0].rel").value("next page"))
+            .andExpect(jsonPath("$.links[0].href").value("http://localhost/api/products?pag=1-5"));
+        }
+
+        @Test
+        @DisplayName("GET "+DEFAULT_URL+"?pag=1-5 - 200 OK")
+        void should_return_the_middle_page_of_paged_products_with_200() throws Exception {
+            final String secondPage = "1-5";
+            final String urlBasePrices = "http://localhost/api/prices?barcode=";
+            final String urlBaseSelf = "http://localhost/api/products/";
+
+            mockMvc.perform(get(DEFAULT_URL).queryParam("pag", secondPage)
+                .characterEncoding(StandardCharsets.UTF_8)
+                .contentType(MediaType.APPLICATION_JSON)
+            )
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$.content").isArray())
+            .andExpect(jsonPath("$.content", hasSize(5)))
+            .andExpect(jsonPath("$.currentPage").value(1))
+            .andExpect(jsonPath("$.totalPages").value(3))
+            .andExpect(jsonPath("$.numberOfItems").value(5))
+            .andExpect(jsonPath("$.hasNext").value(true))
+            .andExpect(
+                jsonPath("$.content[*].barcode",
+                contains("7891962047560", "7896656800018", "7896004004501", "7891098010575", "7896036093085"))
+            )
+            .andExpect(jsonPath("$.content[*].links[0].rel", everyItem(is("prices"))))
+            .andExpect(jsonPath("$.content[*].links[1].rel", everyItem(is("self"))))
+            .andExpect(jsonPath(
+                "$.content[*].links[0].href",
+                contains(
+                    urlBasePrices+"7891962047560", urlBasePrices+"7896656800018",
+                    urlBasePrices+"7896004004501", urlBasePrices+"7891098010575", urlBasePrices+"7896036093085"
+                )
+            ))
+            .andExpect(jsonPath(
+                "$.content[*].links[1].href",
+                contains(
+                    urlBaseSelf+"7891962047560", urlBaseSelf+"7896656800018",
+                    urlBaseSelf+"7896004004501", urlBaseSelf+"7891098010575", urlBaseSelf+"7896036093085"
+                )
+            ))
+            .andExpect(jsonPath("$.links[0].rel").value("next page"))
+            .andExpect(jsonPath("$.links[0].href").value("http://localhost/api/products?pag=2-5"));
+        }
+
+        @Test
+        @DisplayName("GET "+DEFAULT_URL+"?pag=2-5 - 200 OK")
+        void should_return_the_last_page_of_paged_products_with_200() throws Exception {
+            final String thirdPage = "2-5";
+
+            mockMvc.perform(get(DEFAULT_URL).queryParam("pag", thirdPage)
+                .characterEncoding(StandardCharsets.UTF_8)
+                .contentType(MediaType.APPLICATION_JSON)
+            )
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$.content").isArray())
+            .andExpect(jsonPath("$.content", hasSize(1)))
+            .andExpect(jsonPath("$.currentPage").value(2))
+            .andExpect(jsonPath("$.totalPages").value(3))
+            .andExpect(jsonPath("$.numberOfItems").value(1))
+            .andExpect(jsonPath("$.hasNext").value(false))
+            .andExpect(jsonPath("$.content[*].barcode", contains("7891962057620")))
+            .andExpect(jsonPath("$.content[*].links[0].rel").value("prices"))
+            .andExpect(jsonPath("$.content[*].links[1].rel").value("self"))
+            .andExpect(jsonPath(
+                "$.content[*].links[0].href",
+                contains("http://localhost/api/prices?barcode=7891962057620")
+            ))
+            .andExpect(jsonPath(
+                "$.content[*].links[1].href",
+                contains("http://localhost/api/products/7891962057620")
+            ))
+            .andExpect(jsonPath("$.links").isArray())
+            .andExpect(jsonPath("$.links").isEmpty());
+        }
+
+        @Test
+        @DisplayName("GET "+DEFAULT_URL+"?pag=3-5 - 200 OK")
+        void should_not_return_anything_with_200() throws Exception {
+            final String thirdPage = "3-5";
+
+            mockMvc.perform(get(DEFAULT_URL).queryParam("pag", thirdPage)
+                .characterEncoding(StandardCharsets.UTF_8)
+                .contentType(MediaType.APPLICATION_JSON)
+            )
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$").isArray())
+            .andExpect(jsonPath("$").isEmpty());
+        }
     }
 
-    @Test
-    void when_GET_getAll_should_response_the_first_page_of_paged_products_with_200() throws Exception {
-        final String firstPage = "0-5";
+    @Nested
+    class GetByBarcodeTest {
 
-        mockMvc.perform(get(DEFAULT_URL).queryParam("pag", firstPage)
-            .characterEncoding(StandardCharsets.UTF_8)
-            .contentType(MediaType.APPLICATION_JSON)
-        )
-        .andExpect(status().isOk())
-        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-        .andExpect(jsonPath("$.content").isArray())
-        .andExpect(jsonPath("$.content", hasSize(5)))
-        .andExpect(jsonPath("$.currentPage").value(0))
-        .andExpect(jsonPath("$.totalPages").value(3))
-        .andExpect(jsonPath("$.numberOfItems").value(5))
-        .andExpect(jsonPath("$.hasNext").value(true))
-        .andExpect(
-            jsonPath("$.content[*].barcode",
-            contains("7891000055120", "7897534852624", "7896336010058", "7898279792299", "7896045104482"))
-        )
-        .andExpect(jsonPath("$.content[*].prices[*]", hasSize(38)));
+        @Test
+        @DisplayName("GET "+DEFAULT_URL+"/7891000055120 - 200 OK")
+        void should_return_a_product_with_200() throws Exception {
+            final String barcode = "7891000055120";
+
+            mockMvc.perform(get(DEFAULT_URL+"/"+barcode)
+                .characterEncoding(StandardCharsets.UTF_8)
+            )
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$.description").value("ACHOC PO NESCAU 800G"))
+            .andExpect(jsonPath("$.sequenceCode").value(29250))
+            .andExpect(jsonPath("$.barcode").value(barcode))
+            .andExpect(jsonPath("$.links[0].rel").value("prices"))
+            .andExpect(jsonPath("$.links[0].href").value("http://localhost/api/prices?barcode=7891000055120"))
+            .andExpect(jsonPath("$.links[1].rel").value("self"))
+            .andExpect(jsonPath("$.links[1].href").value("http://localhost/api/products/7891000055120"));
+        }
+
+        @Test
+        @DisplayName("GET "+DEFAULT_URL+"/7891000055129 - 404 NOT FOUND")
+        void should_return_an_error_message_with_404() throws Exception {
+            final String nonExistingBarcode = "7891000055129";
+
+            mockMvc.perform(get(DEFAULT_URL+"/"+nonExistingBarcode)
+                .characterEncoding(StandardCharsets.UTF_8)
+                .contentType(MediaType.ALL)
+            )
+            .andExpect(status().isNotFound())
+            .andExpect(content().contentType(MediaType.TEXT_PLAIN))
+            .andExpect(content().string("Product not found"));
+        }
+
+        @Test
+        @DisplayName("GET "+DEFAULT_URL+"/7890foobar - 400 BAD REQUEST")
+        void should_return_violations_with_400() throws Exception {
+            final String violatedBarcode = "7890foobar";
+
+            mockMvc.perform(get(DEFAULT_URL+"/"+violatedBarcode)
+                .accept(MediaType.ALL)
+            )
+            .andExpect(status().isBadRequest())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$.violations").isArray())
+            .andExpect(jsonPath("$.violations", hasSize(2)))
+            .andExpect(jsonPath("$.violations[*].field", everyItem(is(violatedBarcode))))
+            .andExpect(jsonPath(
+                "$.violations[*].violationMessage",
+                containsInAnyOrder("barcode must has 13 characters", "barcode must contain only numbers")
+            ));
+        }
     }
 
-    @Test
-    void when_GET_getAll_should_response_the_middle_page_of_paged_products_with_200() throws Exception {
-        final String middlePag = "1-5";
-
-        mockMvc.perform(get(DEFAULT_URL).queryParam("pag", middlePag)
-            .characterEncoding(StandardCharsets.UTF_8)
-            .contentType(MediaType.APPLICATION_JSON)
-        )
-        .andExpect(status().isOk())
-        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-        .andExpect(jsonPath("$.content").isArray())
-        .andExpect(jsonPath("$.content", hasSize(5)))
-        .andExpect(jsonPath("$.currentPage").value(1))
-        .andExpect(jsonPath("$.totalPages").value(3))
-        .andExpect(jsonPath("$.numberOfItems").value(5))
-        .andExpect(jsonPath("$.hasNext").value(true))
-        .andExpect(
-            jsonPath("$.content[*].barcode",
-            contains("7891962047560", "7896656800018", "7896004004501", "7891098010575", "7896036093085"))
-        )
-        .andExpect(jsonPath("$.content[*].prices[*]", hasSize(27)));
-    }
-
-    @Test
-    void when_GET_getAll_should_response_the_last_page_of_paged_products_with_200() throws Exception {
-        final String lastPage = "2-5";
-
-        mockMvc.perform(get(DEFAULT_URL).queryParam("pag", lastPage)
-            .characterEncoding(StandardCharsets.UTF_8)
-            .contentType(MediaType.APPLICATION_JSON)
-        )
-        .andExpect(status().isOk())
-        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-        .andExpect(jsonPath("$.content").isArray())
-        .andExpect(jsonPath("$.content", hasSize(1)))
-        .andExpect(jsonPath("$.currentPage").value(2))
-        .andExpect(jsonPath("$.totalPages").value(3))
-        .andExpect(jsonPath("$.numberOfItems").value(1))
-        .andExpect(jsonPath("$.hasNext").value(false))
-        .andExpect(jsonPath("$.content[*].barcode", contains("7891962057620")))
-        .andExpect(jsonPath("$.content[*].prices[*]", hasSize(1)));
-    }
-
-    @Test
-    void when_GET_getAll_violating_parameter_pag_should_response_400() throws Exception {
-        final String violatedPag = "0-)";
-
-        mockMvc.perform(get(DEFAULT_URL).queryParam("pag", violatedPag)
-            .characterEncoding(StandardCharsets.UTF_8)
-            .contentType(MediaType.APPLICATION_JSON)
-        )
-        .andExpect(status().isBadRequest())
-        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-        .andExpect(jsonPath("$.violations").isArray())
-        .andExpect(jsonPath("$.violations", hasSize(1)))
-        .andExpect(jsonPath("$.violations[0].field").value(violatedPag))
-        .andExpect(jsonPath("$.violations[0].violationMessage").value("pag must match digit-digit"));
-    }
-
-    @Test
-    void when_GET_getByBarcode_should_response_a_product_with_200() throws Exception {
-        final String barcode = "7891000055120";
-
-        mockMvc.perform(get(DEFAULT_URL+"/"+barcode)
-            .characterEncoding(StandardCharsets.UTF_8)
-        )
-        .andExpect(status().isOk())
-        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-        .andExpect(jsonPath("$.description").value("ACHOC PO NESCAU 800G"))
-        .andExpect(jsonPath("$.sequenceCode").value(29250))
-        .andExpect(jsonPath("$.barcode").value(barcode))
-        .andExpect(jsonPath("$.prices").isArray())
-        .andExpect(jsonPath("$.prices", hasSize(10)))
-        .andExpect(jsonPath("$.prices[*].value", contains(12.70, 19.00, 16.50, 6.61, 16.80, 9.85, 10.60, 16.10, 12.60, 19.10)));
-    }
-
-    @Test
-    void when_GET_getByBarcode_where_barcode_does_not_exist_should_response_not_found() throws Exception {
-        final String nonExistingBarcode = "7891000055129";
-
-        mockMvc.perform(get(DEFAULT_URL+"/"+nonExistingBarcode)
-            .characterEncoding(StandardCharsets.UTF_8)
-        )
-        .andExpect(status().isNotFound())
-        .andExpect(content().encoding(StandardCharsets.UTF_8))
-        .andExpect(content().contentType(MediaType.TEXT_PLAIN_VALUE+";charset=UTF-8"))
-        .andExpect(content().string("Product not found"));
-    }
-
-    @Test
-    void when_GET_getByBarcode_where_barcode_is_violated_should_response_bad_request() throws Exception {
-        final String violatedBarcode = "7890foobar";
-
-        mockMvc.perform(get(DEFAULT_URL+"/"+violatedBarcode)
-            .characterEncoding(StandardCharsets.UTF_8)
-        )
-        .andExpect(status().isBadRequest())
-        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-        .andExpect(jsonPath("$.violations").isArray())
-        .andExpect(jsonPath("$.violations", hasSize(2)))
-        .andExpect(jsonPath("$.violations[0].field").value(violatedBarcode))
-        .andExpect(jsonPath("$.violations[1].field").value(violatedBarcode))
-        .andExpect(jsonPath("$.violations[1].violationMessage").value("barcode must has 13 characters"))
-        .andExpect(jsonPath("$.violations[0].violationMessage").value("barcode must contain only numbers"));
-    }
 }
