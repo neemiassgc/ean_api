5d8d0df3b9c49046e6b47184b2cb7aba5c53e065
Neemias Santos <neemiassgc@outlook.com>
2022-08-08 01:34:18 +0100

    Add a new test method named should_return_all_prices_for_a_barcode to the PriceControllerTest test class

--- src/test/java/com/api/controller/PriceControllerTest.java
+++ src/test/java/com/api/controller/PriceControllerTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -107,5 +108,28 @@ public class PriceControllerTest {
 
         verify(priceRepository, times(1)).findById(eq(uuid));
         verify(priceRepository, only()).findById(eq(uuid));
+    }
+
+    @Test
+    @DisplayName("GET /api/prices?barcode=7896656800018 - 200 OK")
+    void should_return_all_prices_for_a_barcode() throws Exception {
+        final String barcode = "7896656800018";
+        final Sort defaultSort = Sort.by("instant").descending();
+
+        given(priceRepository.findByProductBarcode(eq(barcode), eq(defaultSort)))
+            .willReturn(usefulPrices);
+
+        mockMvc.perform(get("/api/prices?barcode="+barcode)
+            .characterEncoding(StandardCharsets.UTF_8)
+            .accept(MediaType.ALL)
+        )
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$").isArray())
+        .andExpect(jsonPath("$", hasSize(5)))
+        .andExpect(jsonPath("$[*].value", contains(34.5, 4.52, 16.75, 12.12, 6.39)));
+
+        verify(priceRepository, times(1)).findByProductBarcode(eq(barcode), eq(defaultSort));
+        verify(priceRepository, only()).findByProductBarcode(eq(barcode), eq(defaultSort));
     }
 }
