18e3888bace7d1076e8f18ffbacb90691286dee4
Neemias Santos <neemiassgc@outlook.com>
2022-08-08 02:38:14 +0100

    Add a new test method named should_return_a_price_by_its_id_with_200 to the PriceControllerIT test class

--- src/test/java/com/api/controller/PriceControllerIT.java
+++ src/test/java/com/api/controller/PriceControllerIT.java
@@ -1,10 +1,18 @@
 package com.api.controller;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.nio.charset.StandardCharsets;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -13,4 +21,17 @@ public class PriceControllerIT {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("GET /api/prices/5ad12c1a-2103-407c-adcc-832e3f99fb5b - 200 OK")
+    void should_return_a_price_by_its_id_with_200() throws Exception {
+        mockMvc.perform(get("/api/prices/5ad12c1a-2103-407c-adcc-832e3f99fb5b")
+            .characterEncoding(StandardCharsets.UTF_8)
+            .accept(MediaType.ALL)
+        )
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$.value").value(18.40))
+        .andExpect(jsonPath("$.instant").value("2021-09-09T16:50:10.554Z"));
+    }
 }
