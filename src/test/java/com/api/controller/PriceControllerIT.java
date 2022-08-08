113100eba0fd3285247bcec687a6a420d43b2c63
Neemias Santos <neemiassgc@outlook.com>
2022-08-08 02:32:09 +0100

    Create a new test class called PriceControllerIT and setup it

---
+++ src/test/java/com/api/controller/PriceControllerIT.java
@@ -0,0 +1,16 @@
+package com.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE) // disable in-memory database
+public class PriceControllerIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+}
