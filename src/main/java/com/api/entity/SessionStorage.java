c94fd1ef9f53a2dea108c99a14e9793cba0f73fe
Neemias Santos <neemiassgc@outlook.com>
2022-08-11 21:14:34 +0100

    Add more persistence features to the SessionStorage entity

--- src/main/java/com/api/entity/SessionStorage.java
+++ src/main/java/com/api/entity/SessionStorage.java
@@ -2,9 +2,7 @@ package com.api.entity;
 
 import lombok.*;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
@@ -16,16 +14,27 @@ import java.time.LocalDate;
 public class SessionStorage {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SessionStorageSequence")
+    @SequenceGenerator(
+        name = "SessionStorageSequence",
+        sequenceName = "session_storage_seq"
+    )
+    @Setter(value = AccessLevel.NONE)
+    private Long id;
+
+    @Column(nullable = false)
     private Long instance;
 
-    @Column(length = 100)
+    @Column(length = 100, nullable = false)
     private String cookieKey;
 
-    @Column(length = 100)
+    @Column(length = 100, nullable = false)
     private String cookieValue;
 
+    @Column(nullable = false)
     private String ajaxId;
 
+    @Column(nullable = false)
     private LocalDate creationDate;
 
     @Override
