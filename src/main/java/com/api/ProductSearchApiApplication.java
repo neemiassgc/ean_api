3c7a40bba0469720f374478227472c23a52d89c0
Neemias Santos <neemiassgc@outlook.com>
2022-08-11 01:44:20 +0100

    Rename the name of Application from saveg-barcode-querier to saveg-product-search-api

--- src/main/java/com/api/ProductSearchApiApplication.java
+++ src/main/java/com/api/ProductSearchApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BarcodeSearchApplication {
+public class ProductSearchApiApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(BarcodeSearchApplication.class, args);
+		SpringApplication.run(ProductSearchApiApplication.class, args);
 	}
 }
