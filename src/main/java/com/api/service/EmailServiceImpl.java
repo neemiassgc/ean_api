4fc0754af2e15773b85e314a30b62a385789c846
Neemias Santos <neemiassgc@outlook.com>
2022-08-14 23:46:09 +0100

    Refactor EmailServiceImpl class for sending email correctly

--- src/main/java/com/api/service/EmailServiceImpl.java
+++ src/main/java/com/api/service/EmailServiceImpl.java
@@ -1,9 +1,14 @@
 package com.api.service;
 
+import com.api.pojo.Constants;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 @Service
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
@@ -14,7 +19,15 @@ public class EmailServiceImpl implements EmailService {
     private final JavaMailSender javaMailSender;
 
     @Override
-    public void sendAuditEmail(String message) {
+    public void sendAuditEmail(final String message) {
+        final SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setFrom(OWNER_EMAIL);
+        simpleMailMessage.setTo(OWNER_EMAIL);
+        simpleMailMessage.setSubject(
+            "Audit email sent with JavaMail - "+LocalDate.now(ZoneId.of(Constants.TIMEZONE))
+        );
+        simpleMailMessage.setText(message);
 
+        javaMailSender.send(simpleMailMessage);
     }
 }
