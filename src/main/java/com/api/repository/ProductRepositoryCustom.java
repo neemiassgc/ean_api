7a661e888740481dcf6a84b7c6b7dfe9ac6f3d7b
Neemias Santos <neemiassgc@outlook.com>
2022-08-16 03:28:30 +0100

    Delete ProductRepositoryCustom interface

--- src/main/java/com/api/repository/ProductRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package com.api.repository;
-
-import com.api.entity.Product;
-import lombok.NonNull;
-
-public interface ProductRepositoryCustom {
-
-    Product processByBarcode(@NonNull final String barcode);
-}
