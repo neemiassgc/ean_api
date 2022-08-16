814f19ef2ecdcbd27a8be65fefc403bf363ffde5
Neemias Santos <neemiassgc@outlook.com>
2022-08-16 03:17:11 +0100

    Create a new interface called SessionStorageService

---
+++ src/main/java/com/api/service/interfaces/SessionStorageService.java
@@ -0,0 +1,11 @@
+package com.api.service.interfaces;
+
+import com.api.entity.SessionStorage;
+import org.springframework.data.domain.Sort;
+
+import java.util.Optional;
+
+public interface SessionStorageService {
+
+    Optional<SessionStorage> findTopBy(Sort sort);
+}
