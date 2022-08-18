d0b72d4d66af2a34dd0daf8e49b00b6444bdf762
Neemias Santos <neemiassgc@outlook.com>
2022-08-18 16:45:25 +0100

    Configure a Bean called restTemplate in the BeansConfiguration class

--- src/main/java/com/api/configuration/BeansConfiguration.java
+++ src/main/java/com/api/configuration/BeansConfiguration.java
@@ -1,17 +1,25 @@
 package com.api.configuration;
 
 import org.apache.http.client.CookieStore;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.impl.client.BasicCookieStore;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.DefaultRedirectStrategy;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 
 import javax.mail.Authenticator;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
+import java.time.Duration;
 import java.util.Locale;
 import java.util.Properties;
 
@@ -53,5 +61,27 @@ public class BeansConfiguration {
     @Bean
     public CookieStore cookieStore() {
         return new BasicCookieStore();
+    }
+
+    @Bean
+    public RestTemplate restTemplate(final RestTemplateBuilder restTemplateBuilder) {
+        final int twoSeconds = (int) Duration.ofSeconds(2).toMillis();
+
+        final RequestConfig requestConfig = RequestConfig
+            .custom()
+            .setConnectionRequestTimeout(twoSeconds)
+            .setConnectTimeout(twoSeconds)
+            .build();
+
+        final CloseableHttpClient httpClient = HttpClientBuilder.create()
+            .setDefaultRequestConfig(requestConfig)
+            .setRedirectStrategy(DefaultRedirectStrategy.INSTANCE)
+            .setDefaultCookieStore(cookieStore())
+            .build();
+
+        return restTemplateBuilder
+            .rootUri("https://apex.savegnago.com.br/apexmobile")
+            .requestFactory(() -> new HttpComponentsClientHttpRequestFactory(httpClient))
+            .build();
     }
 }
