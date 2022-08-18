6f04e3d517c44ff89664b23e25bf7375e5b6f150
Neemias Santos <neemiassgc@outlook.com>
2022-08-18 19:07:25 +0100

    Delete method readFromInputStream

--- src/main/java/com/api/pojo/DomainUtils.java
+++ src/main/java/com/api/pojo/DomainUtils.java
@@ -5,12 +5,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.math.BigDecimal;
-import java.util.stream.Collectors;
 
 public final class DomainUtils {
 
@@ -27,14 +22,6 @@ public final class DomainUtils {
 
     public static BigDecimal parsePrice(final String priceInput) {
         return new BigDecimal((priceInput.replace(",", ".")));
-    }
-
-    public static String readFromInputStream(final InputStream inputStream) throws IOException {
-        try (final InputStreamReader isr = new InputStreamReader(inputStream)) {
-            try (final BufferedReader br = new BufferedReader(isr)) {
-                return br.lines().collect(Collectors.joining(""));
-            }
-        }
     }
 
     public static <O> O requireNonNull(final O object, final RuntimeException runtimeException) {
