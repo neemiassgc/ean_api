445045d88c32526ba6e99564d57516e79d7a3cc6
Neemias Santos <neemiassgc@outlook.com>
2022-08-25 02:27:14 +0100

    Change the name of api from saveg-product-search-api to saveg-local-market-api

--- src/main/java/com/api/SavegLocalMarketApplication.java
+++ src/main/java/com/api/SavegLocalMarketApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProductSearchApiApplication {
+public class SavegLocalMarketApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(ProductSearchApiApplication.class, args);
+		SpringApplication.run(SavegLocalMarketApplication.class, args);
 	}
 }
