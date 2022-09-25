bb3cf6f908f1c5b559b47bfe7f22adb7cf7e0f21
Neemias Santos <neemiassgc@outlook.com>
2022-09-25 20:47:15 +0100

    Refactor ProductController to adapt the changes

--- src/main/java/com/api/controller/ProductController.java
+++ src/main/java/com/api/controller/ProductController.java
@@ -1,10 +1,11 @@
 package com.api.controller;
 
 import com.api.annotation.Barcode;
-import com.api.entity.Product;
 import com.api.component.DomainUtils;
+import com.api.entity.Product;
 import com.api.projection.PagedEntity;
-import com.api.service.DomainMapper;
+import com.api.projection.SimpleProduct;
+import com.api.projection.SimpleProductWithStatus;
 import com.api.service.interfaces.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,7 +23,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static com.api.projection.Projection.SimpleProduct;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
@@ -34,28 +34,27 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 public class ProductController {
 
     private final ProductService productService;
-    private final DomainMapper domainMapper;
 
     @GetMapping(path = "/products", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> getAll() {
         List<EntityModel<SimpleProduct>> responseBody =
-            makeMapping(productService.findAll(Sort.by("description").ascending()));
+            makeMappingAndLinks(productService.findAll(Sort.by("description").ascending()));
 
         return ResponseEntity.ok(CollectionModel.of(responseBody));
     }
 
     @GetMapping(path = "/products", params = "pag", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> getAll(@RequestParam(name = "pag") String pag) {
+    public ResponseEntity<?> getPagedAll(@RequestParam(name = "pag") String pag) {
         final Page<Product> productPage =
             productService.findAll(DomainUtils.parsePage(pag, Sort.by("description").ascending()));
 
         if (productPage.getContent().isEmpty()) return ResponseEntity.ok(Collections.emptyList());
 
-        final List<EntityModel<SimpleProduct>> content = makeMapping(productPage.getContent());
-        PagedEntity<EntityModel<SimpleProduct>> pagedModel = new PagedEntity<>(productPage, content);
+        PagedEntity<EntityModel<SimpleProduct>> pagedModel =
+            new PagedEntity<>(productPage, makeMappingAndLinks(productPage.getContent()));
 
         pagedModel.addIf(productPage.hasNext(), () ->
-            linkTo(methodOn(this.getClass()).getAll((productPage.getNumber() + 1)+"-"+productPage.getSize()))
+            linkTo(methodOn(this.getClass()).getPagedAll((productPage.getNumber() + 1)+"-"+productPage.getSize()))
                 .withRel("next page")
         );
 
@@ -64,23 +63,22 @@ public class ProductController {
 
     @GetMapping(path = "/products/{barcode}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> getByBarcode(@PathVariable("barcode") @Barcode String barcode) {
-        final Product productToProcess = productService.getByBarcode(barcode);
-        final SimpleProduct simpleProductToResponse = domainMapper.mapToSimpleProduct(productToProcess);
+        final SimpleProductWithStatus simpleProductWithStatus = productService.getByBarcodeAndSaveIfNecessary(barcode);
 
         final Link linkToPrices = linkTo(methodOn(PriceController.class).searchByProductBarcode(barcode))
             .withRel("prices");
         final Link linkToSelf = linkTo(methodOn(this.getClass()).getByBarcode(barcode)).withSelfRel();
 
-
         final EntityModel<SimpleProduct> responseBody =
-            EntityModel.of(simpleProductToResponse, linkToPrices, linkToSelf);
+            EntityModel.of(simpleProductWithStatus.getSimpleProduct(), linkToPrices, linkToSelf);
 
-        return ResponseEntity.ok(responseBody);
+        return ResponseEntity.status(simpleProductWithStatus.getHttpStatus()).body(responseBody);
     }
 
-    private List<EntityModel<SimpleProduct>> makeMapping(List<Product> inputList) {
-         return domainMapper.mapToSimpleProductList(inputList)
+    private List<EntityModel<SimpleProduct>> makeMappingAndLinks(List<Product> inputList) {
+         return inputList
             .stream()
+            .map(Product::toSimpleProduct)
             .map(it -> {
                 final Link linkToPrices = linkTo(methodOn(PriceController.class)
                     .searchByProductBarcode(it.getBarcode()))
