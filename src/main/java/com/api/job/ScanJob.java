6b13aafbafaeabf41decffe84387efab1dd69620
Neemias Santos <neemiassgc@outlook.com>
2022-09-27 19:29:42 +0100

    Refactor ScanJob

--- src/main/java/com/api/job/ScanJob.java
+++ src/main/java/com/api/job/ScanJob.java
@@ -2,9 +2,11 @@ package com.api.job;
 
 import com.api.entity.Price;
 import com.api.entity.Product;
-import com.api.service.interfaces.EmailService;
 import com.api.service.ProductExternalServiceImpl;
+import com.api.service.interfaces.EmailService;
 import com.api.service.interfaces.ProductService;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.quartz.Job;
@@ -12,13 +14,12 @@ import org.quartz.JobExecutionContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
+import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
@@ -29,55 +30,89 @@ public class ScanJob implements Job {
     private final ProductService productService;
     private final EmailService emailService;
 
-    @Transactional(propagation = Propagation.REQUIRED)
-    private Map<String, Integer> delegateTask() {
-        final List<Product> products = productService.findAllWithLastPrice();
+    @Override
+    public void execute(JobExecutionContext context) {
+        final long startMeasureTime = System.currentTimeMillis();
+        final Info info;
+
+        try {
+            info = updatePrices();
+        }
+        catch (Exception e) {
+            sendFailureMessage(e.getMessage());
+            throw e;
+        }
+        final long elapsedTimeInSeconds = Duration.ofMillis(System.currentTimeMillis() - startMeasureTime).toSeconds();
+
+        sendSuccessMessage(
+            Info.builder()
+                .changedProductDescriptions(info.getChangedProductDescriptions())
+                .countOfChangedProducts(info.getCountOfChangedProducts())
+                .totalOfProducts(info.getTotalOfProducts())
+                .elapsedTimeInSeconds(elapsedTimeInSeconds)
+                .build()
+        );
+    }
+
+    private void sendFailureMessage(final String message) {
+        final String emailMessage =
+        "Running job failed\n\n"+
+        "With exception: "+message;
+
+        emailService.sendAuditEmail(emailMessage);
+    }
+
+    private void sendSuccessMessage(final Info info) {
+        final String emailMessage =
+            "Running job successfully\n\n"+
+            String.format("%d/%d products changed\n\n", info.getCountOfChangedProducts(), info.getTotalOfProducts())+
+            String.format("Elapsed time: %d seconds\n\n", info.getElapsedTimeInSeconds()) +
+            String.join("\n", info.getChangedProductDescriptions());
+
+        emailService.sendAuditEmail(emailMessage);
+    }
+
+    private Info updatePrices() {
+        final List<Product> products = productService.findAllWithLatestPrice();
+        final List<Product> changedProducts = new ArrayList<>();
         final int totalOfProducts = products.size();
         int countOfChangedProducts = 0;
 
         for (Product product : products) {
             final Price newPrice = productExternalServiceImpl.fetchByBarcode(product.getBarcode())
-                .map(it -> {
-                    final Price priceToReturn = it.getPrices().get(0);
-                    it.removePrice(priceToReturn);
-                    return priceToReturn;
+                .map(fetchedProduct -> {
+                    final Price price = fetchedProduct.getPrices().get(0);
+                    fetchedProduct.removePrice(price);
+                    return price;
                 })
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found"));
 
-            if (product.getPrices().get(0).getValue().equals(newPrice.getValue()))
+            if (checkIfPricesAreEqual(product.getPrices().get(0), newPrice))
                 continue;
 
             productService.save(product.addPrice(newPrice));
             countOfChangedProducts++;
+            changedProducts.add(product);
         }
 
-        return Map.of("totalOfProducts", totalOfProducts, "countOfChangedProducts", countOfChangedProducts);
+        return Info.builder()
+            .countOfChangedProducts(countOfChangedProducts)
+            .totalOfProducts(totalOfProducts)
+            .changedProductDescriptions(changedProducts.stream().map(Product::getDescription).collect(Collectors.toList()))
+            .build();
     }
 
-    @Override
-    public void execute(JobExecutionContext context) {
-        final long startMeasureTime = System.currentTimeMillis();
-        Map<String, Integer> summary = null;
+    private boolean checkIfPricesAreEqual(final Price priceA, final Price priceB) {
+        return priceA.getValue().equals(priceB.getValue());
+    }
 
-        try {
-            summary = delegateTask();
-        }
-        catch (Exception e) {
-            final String emailMessage =
-            "Running job failed\n\n"+
-            "With exception: "+e.getMessage();
-
-            emailService.sendAuditEmail(emailMessage);
-
-            throw e;
-        }
-        final long elapsedTimeInSeconds = Duration.ofMillis(System.currentTimeMillis() - startMeasureTime).toSeconds();
-
-        final String emailMessage =
-        "Running job successfully\n\n"+
-        String.format("%d/%d products changed\n\n", summary.get("countOfChangedProducts"), summary.get("totalOfProducts"))+
-        String.format("Elapsed time: %d seconds", elapsedTimeInSeconds);
-
-        emailService.sendAuditEmail(emailMessage);
+    @Getter
+    @RequiredArgsConstructor
+    @Builder
+    private static class Info {
+        private final int countOfChangedProducts;
+        private final int totalOfProducts;
+        private final long elapsedTimeInSeconds;
+        private final List<String> changedProductDescriptions;
     }
 }
