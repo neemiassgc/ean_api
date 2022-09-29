f478c26e7a680616eac77fec034cf5ad7b0fb2f8
Neemias Santos <neemiassgc@outlook.com>
2022-09-29 14:20:48 +0100

    Disable HypermediaAutoConfiguration

--- src/main/java/com/api/SavegLocalMarketApplication.java
+++ src/main/java/com/api/SavegLocalMarketApplication.java
@@ -1,9 +1,12 @@
 package com.api;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.hateoas.HypermediaAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = HypermediaAutoConfiguration.class)
 public class SavegLocalMarketApplication {
 
 	public static void main(String[] args) {
