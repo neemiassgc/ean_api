10849cd93ab7907324b8aefa28c5e25a8c937722
Neemias Santos <neemiassgc@outlook.com>
2022-09-30 16:26:53 +0100

    Add an unit test method called when_id_does_not_exist_then_should_throw_an_exception

--- src/test/java/com/api/service/PriceServiceImplTest.java
+++ src/test/java/com/api/service/PriceServiceImplTest.java
@@ -6,19 +6,23 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.catchThrowable;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
 
 final class PriceServiceImplTest {
 
-    @MockBean
     private PriceRepository priceRepositoryMock;
     private PriceServiceImpl priceServiceUnderTest;
 
     @BeforeEach
     void setup() {
-        priceServiceUnderTest = new PriceServiceImpl(priceRepositoryMock);
+        priceServiceUnderTest = new PriceServiceImpl(priceRepositoryMock = mock(PriceRepository.class));
     }
 
     @Nested
@@ -31,6 +35,25 @@ final class PriceServiceImplTest {
 
             assertThat(actualThrowable).isNotNull();
             assertThat(actualThrowable).isInstanceOf(NullPointerException.class);
+        }
+
+        @Test
+        @DisplayName("Should throw ResponseStatusException with NOT FOUND EXCEPTION")
+        void when_id_does_not_exist_then_should_throw_an_exception() {
+            final UUID nonExistentId = UUID.fromString("4843ca41-2532-4247-bae4-16e61b8108cc");
+            given(priceRepositoryMock.findById(eq(nonExistentId))).willReturn(Optional.empty());
+
+            final Throwable actualThrowable = catchThrowable(() -> priceServiceUnderTest.findById(nonExistentId));
+
+            assertThat(actualThrowable).isNotNull();
+            assertThat(actualThrowable).isInstanceOf(ResponseStatusException.class);
+            assertThat((ResponseStatusException) actualThrowable).satisfies(exception -> {
+                assertThat(exception.getReason()).isEqualTo("Price not found");
+                assertThat(exception.getStatus()).isEqualTo(HttpStatus.NOT_FOUND);
+            });
+
+            verify(priceRepositoryMock, times(1)).findById(eq(nonExistentId));
+            verify(priceRepositoryMock, only()).findById(eq(nonExistentId));
         }
     }
 
