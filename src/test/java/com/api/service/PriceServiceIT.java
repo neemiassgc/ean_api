5f27421e04a8ccab1db2baab3e26909b4860d95d
Neemias Santos <neemiassgc@outlook.com>
2022-10-04 18:30:24 +0100

    Create an integration test called when_id_exist_the_should_return_a_price

--- src/test/java/com/api/service/PriceServiceIT.java
+++ src/test/java/com/api/service/PriceServiceIT.java
@@ -1,20 +1,38 @@
 package com.api.service;
 
-import com.api.repository.PriceRepository;
+import com.api.entity.Price;
+import com.api.service.interfaces.PriceService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional(readOnly = true)
 public class PriceServiceIT {
 
     @Autowired
-    private PriceRepository priceRepository;
+    private PriceService priceService;
 
     @Nested
     final class FindByIdTest {
 
+        @Test
+        @DisplayName("Should return a price")
+        void when_id_exist_the_should_return_a_price() {
+            final UUID existentUuid = UUID.fromString("9423f8be-2a4f-4baa-b457-6a904bf633f0");
+
+            final Price actualPrice = priceService.findById(existentUuid);
+
+            assertThat(actualPrice).isNotNull();
+            assertThat(actualPrice.getValue()).isEqualTo(new BigDecimal("12.70"));
+        }
     }
 }
