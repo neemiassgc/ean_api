4f725adcdc9050323b0053dc5d839c4f21488d45
Neemias Santos <neemiassgc@outlook.com>
2022-10-05 00:59:17 +0100

    Clear class

--- src/test/java/com/api/service/ProductServiceIT.java
+++ src/test/java/com/api/service/ProductServiceIT.java
@@ -1,25 +1,14 @@
 package com.api.service;
 
-import com.api.entity.Product;
-import com.api.projection.SimpleProductWithStatus;
 import com.api.repository.PriceRepository;
 import com.api.repository.ProductRepository;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.server.ResponseStatusException;
-
-import java.util.UUID;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.catchThrowable;
 
 @SpringBootTest
 @Transactional(readOnly = true)
-public class ProductServiceImplIT {
+public class ProductServiceIT {
 
     @Autowired
     private ProductServiceImpl productServiceImplUnderTest;
