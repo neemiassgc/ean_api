878fa956176700bafb58a68caf848582d64f8a5d
Neemias Santos <neemiassgc@outlook.com>
2022-10-04 22:56:51 +0100

    Refactor to clean the test class and setup

--- src/test/java/com/api/service/ProductServiceImplTest.java
+++ src/test/java/com/api/service/ProductServiceImplTest.java
@@ -1,107 +1,24 @@
 package com.api.service;
 
-import com.api.entity.Product;
 import com.api.repository.ProductRepository;
+import com.api.service.interfaces.ProductExternalService;
+import com.api.service.interfaces.ProductService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestInstance;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import org.junit.jupiter.api.*;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
-
-import java.util.Optional;
-
-import static org.assertj.core.api.Assertions.catchThrowable;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ProductServiceImplTest {
 
-    private ProductServiceImpl productServiceImplUnderTest;
-    private ProductRepository productRepository;
-    private ProductExternalServiceImpl productExternalServiceImpl;
-
-    // resources for testing
-    private Product defaultProduct;
-
-    @BeforeAll
-    void init() {
-        this.defaultProduct = Product.builder()
-            .description("OLEO MARIA")
-            .barcode("7896036093085")
-            .sequenceCode(1184)
-            .build();
-    }
+    private ProductService productServiceImplUnderTest;
+    private ProductRepository productRepositoryMock;
+    private ProductExternalService productExternalServiceMock;
 
     @BeforeEach
-    void preset() {
-        this.productRepository = mock(ProductRepository.class);
-        this.productExternalServiceImpl = mock(ProductExternalServiceImpl.class);
-        this.productServiceImplUnderTest = new ProductServiceImpl(this.productRepository, this.productExternalServiceImpl);
-    }
-
-    @DisplayName("When a product already exists in DB then returns it - processByBarcode")
-    @Test
-    void should_return_a_product_from_db() {
-        // given
-        final String targetBarcode = defaultProduct.getBarcode();
-        given(productRepository.findByBarcode(eq(targetBarcode)))
-            .willReturn(Optional.of(defaultProduct));
-
-        // when
-        final Product actualProduct = productServiceImplUnderTest.getByBarcode(targetBarcode);
-
-        // then
-       assertThat(actualProduct).isEqualTo(defaultProduct);
-
-       verify(productRepository, times(1)).findByBarcode(eq(targetBarcode));
-       verify(productRepository, only()).findByBarcode(eq(targetBarcode));
-    }
-
-    @DisplayName("When a product does not exist in DB then checks in the external service  - processByBarcode")
-    @Test
-    void should_return_a_product_from_the_external_service() {
-        // given
-        final String targetBarcode = defaultProduct.getBarcode();
-        given(productRepository.findByBarcode(eq(targetBarcode))).willReturn(Optional.empty());
-        given(productExternalServiceImpl.fetchByBarcode(eq(targetBarcode)))
-            .willReturn(Optional.of(defaultProduct));
-        given(productRepository.save(eq(defaultProduct)))
-            .willAnswer(invocation ->  invocation.getArgument(0, Product.class));
-
-        // when
-        final Product actualProduct = productServiceImplUnderTest.getByBarcode(targetBarcode);
-
-        // then
-        assertThat(actualProduct).isEqualTo(defaultProduct);
-
-        verify(productRepository, times(1)).findByBarcode(eq(targetBarcode));
-        verify(productExternalServiceImpl, times(1)).fetchByBarcode(eq(targetBarcode));
-        verify(productRepository, times(1)).save(eq(defaultProduct));
-    }
-
-    @DisplayName("When a product does not exist anywhere then throws an exception - processByBarcode")
-    @Test
-    void should_throw_an_exception() {
-        // given
-        final String targetBarcode = defaultProduct.getBarcode();
-        given(productRepository.findByBarcode(eq(targetBarcode))).willReturn(Optional.empty());
-        given(productExternalServiceImpl.fetchByBarcode(eq(targetBarcode))).willReturn(Optional.empty());
-
-        // when
-        final Throwable actualException =
-            catchThrowable(() -> productServiceImplUnderTest.getByBarcode(targetBarcode));
-
-        // then
-        assertThat(actualException).isNotNull();
-        assertThat(actualException).isInstanceOf(ResponseStatusException.class);
-        assertThat(((ResponseStatusException)actualException).getStatus()).isEqualTo(HttpStatus.NOT_FOUND);
-        assertThat(((ResponseStatusException)actualException).getReason()).isEqualTo("Product not found");
-
-        verify(productRepository, times(1)).findByBarcode(eq(targetBarcode));
-        verify(productExternalServiceImpl, times(1)).fetchByBarcode(eq(targetBarcode));
-        verify(productRepository, never()).save(eq(defaultProduct));
+    void setup() {
+        productExternalServiceMock = mock(ProductExternalService.class);
+        productRepositoryMock = mock(ProductRepository.class);
+        productServiceImplUnderTest = new ProductServiceImpl(productRepositoryMock, productExternalServiceMock);
     }
 }
