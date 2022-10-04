c4ef817a4f2450a399dd7c7a5f96b2a55903c5d2
Neemias Santos <neemiassgc@outlook.com>
2022-10-04 23:25:35 +0100

    Create a resource static class called Resources

--- src/test/java/com/api/service/ProductServiceTest.java
+++ src/test/java/com/api/service/ProductServiceTest.java
@@ -1,10 +1,15 @@
 package com.api.service;
 
+import com.api.entity.Price;
+import com.api.entity.Product;
 import com.api.repository.ProductRepository;
 import com.api.service.interfaces.ProductExternalService;
 import com.api.service.interfaces.ProductService;
 import org.hibernate.validator.constraints.time.DurationMax;
 import org.junit.jupiter.api.*;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
@@ -26,5 +31,43 @@ public class ProductServiceTest {
 
     @Nested
     class GetByBarcodeAndSaveIfNecessaryTest {
+    }
+
+    private static final class Resources {
+
+        private static final List<Product> PRODUCT_LIST = List.of(
+            Product.builder()
+                .description("ACHOC PO NESCAU 800G")
+                .barcode("7891000055120")
+                .sequenceCode(29250)
+                .build(),
+            Product.builder()
+                .description("AMENDOIM SALG CROKISSIMO 400G PIMENTA")
+                .barcode("7896336010058")
+                .sequenceCode(120983)
+                .build(),
+            Product.builder()
+                .description("BALA GELATINA FINI 500G BURGUER")
+                .barcode("7896336010058")
+                .sequenceCode(93556)
+                .build()
+        );
+
+        static {
+            PRODUCT_LIST.get(0)
+                .addPrice(new Price(new BigDecimal("16.98")))
+                .addPrice(new Price(new BigDecimal("11.54")))
+                .addPrice(new Price(new BigDecimal("9")));
+
+            PRODUCT_LIST.get(1)
+                .addPrice(new Price(new BigDecimal("5.2")))
+                .addPrice(new Price(new BigDecimal("1.39")))
+                .addPrice(new Price(new BigDecimal("8.75")));
+
+            PRODUCT_LIST.get(2)
+                .addPrice(new Price(new BigDecimal("6.11")))
+                .addPrice(new Price(new BigDecimal("2.49")))
+                .addPrice(new Price(new BigDecimal("6.96")));
+        }
     }
 }
