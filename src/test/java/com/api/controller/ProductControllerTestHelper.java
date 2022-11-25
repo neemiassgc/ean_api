097f9cd74d379217e8cfeb8612b70bf79ecb55d5
Neemias Santos <neemiassgc@outlook.com>
2022-11-25 13:11:11 +0000

    test: Create a constant that keeps a products sample called PRODUCTS_SAMPLE

--- src/test/java/com/api/controller/ProductControllerTestHelper.java
+++ src/test/java/com/api/controller/ProductControllerTestHelper.java
@@ -1,11 +1,13 @@
 package com.api.controller;
 
+import com.api.entity.Product;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 import java.util.stream.Stream;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -45,4 +47,27 @@ final class ProductControllerTestHelper {
     static ResultActions makeRequestWithPageAndContains(final String page, final String contains) throws Exception {
         return mockMvc.perform(setupRequestHeaders(get(String.format("%s?pag=%s&contains=%s", URL, page, contains))));
     }
+
+    static final List<Product> PRODUCTS_SAMPLE = List.of(
+        Product.builder()
+            .description("ACHOC PO NESCAU 800G")
+            .sequenceCode(29250)
+            .barcode("7891000055120")
+            .build(),
+        Product.builder()
+            .description("AMENDOIM SALG CROKISSIMO 400G PIMENTA")
+            .sequenceCode(120983)
+            .barcode("7896336010058")
+            .build(),
+        Product.builder()
+            .description("CAFE UTAM 500G")
+            .sequenceCode(2909)
+            .barcode("7896656800018")
+            .build(),
+        Product.builder()
+            .description("BALA GELATINA FINI 500G BURGUER")
+            .barcode("78982797922990")
+            .sequenceCode(93556)
+            .build()
+    );
 }
