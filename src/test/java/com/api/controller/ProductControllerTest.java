1f5ebda9db82c459109975aa7d323e2e0bbedd6c
Neemias Santos <neemiassgc@outlook.com>
2022-12-11 16:25:28 +0000

    test: Rename the test to should_return_the_fist_page_with_five_products__OK and make it pass

--- src/test/java/com/api/controller/ProductControllerTest.java
+++ src/test/java/com/api/controller/ProductControllerTest.java
@@ -106,31 +106,40 @@ class ProductControllerTest {
     class GetAllPagedTest {
 
         @Test
-        @DisplayName("GET /api/products?pag=0-1 -> 200 OK")
-        void should_return_the_fist_page_with_one_product__OK() throws Exception  {
-            final Pageable firstPageOrderedByDescriptionAsc = PageRequest.of(0, 1, Sort.by("description").ascending());
-            final List<Product> firstProduct = PRODUCTS_SAMPLE.subList(0, 1);
+        @DisplayName("GET /api/products?pag=0-5 -> 200 OK")
+        void should_return_the_fist_page_with_five_products__OK() throws Exception  {
+            final Pageable firstPageOrderedByDescriptionAsc = PageRequest.of(0, 5, Sort.by("description").ascending());
+            final List<Product> topFiveProducts = PRODUCTS_SAMPLE.subList(0, 5);
 
             given(productService.findAll(eq(firstPageOrderedByDescriptionAsc)))
-                .willReturn(new PageImpl<>(firstProduct, firstPageOrderedByDescriptionAsc, 3));
+                .willReturn(new PageImpl<>(topFiveProducts, firstPageOrderedByDescriptionAsc, 15));
 
-            makeRequestWithPage("0-1")
+            final String[] expectedBarcodeList = {
+                "7891000055120", "7896336010058",
+                "78982797922990", "7896003737257",
+                "7896071024709"
+            };
+            makeRequestWithPage("0-5")
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.content[0].description").value("ACHOC PO NESCAU 800G"))
-                .andExpect(jsonPath("$.content[0].sequenceCode").value(29250))
-                .andExpect(jsonPath("$.content[0].barcode").value("7891000055120"))
-                .andExpect(jsonPath("$.content[0].links[0].rel").value("prices"))
-                .andExpect(jsonPath("$.content[0].links[1].rel").value("self"))
-                .andExpect(jsonPath("$.content[0].links[0].href").value("http://localhost/api/prices?barcode=7891000055120"))
-                .andExpect(jsonPath("$.content[0].links[1].href").value("http://localhost/api/products/7891000055120"))
-                .andExpect(jsonPath("$.currentCountOfItems").value(1))
+                .andExpect(jsonPath("$.content[*].barcode", contains(expectedBarcodeList)))
+                .andExpect(jsonPath("$.content[*].links[0].rel", everyItem(equalTo("prices"))))
+                .andExpect(jsonPath("$.content[*].links[1].rel", everyItem(equalTo("self"))))
+                .andExpect(jsonPath(
+                    "$.content[*].links[0].href",
+                    contains(concatWithUrl("http://localhost/api/prices?barcode=", expectedBarcodeList))
+                ))
+                .andExpect(jsonPath(
+                    "$.content[*].links[1].href",
+                    contains(concatWithUrl("http://localhost/api/products/", expectedBarcodeList))
+                ))
+                .andExpect(jsonPath("$.currentCountOfItems").value(5))
                 .andExpect(jsonPath("$.currentPage").value(0))
                 .andExpect(jsonPath("$.hasNext").value(true))
                 .andExpect(jsonPath("$.totalOfPages").value(3))
-                .andExpect(jsonPath("$.totalOfItems").value(3))
+                .andExpect(jsonPath("$.totalOfItems").value(15))
                 .andExpect(jsonPath("$.links[0].rel").value("Next page"))
-                .andExpect(jsonPath("$.links[0].href").value("http://localhost/api/products?pag=1-1"));
+                .andExpect(jsonPath("$.links[0].href").value("http://localhost/api/products?pag=1-5"));
 
             verify(productService, times(1)).findAll(eq(firstPageOrderedByDescriptionAsc));
         }
