fb8d50c67e906de417bc407147fb276f448bd2e4
Neemias Santos <neemiassgc@outlook.com>
2022-12-13 14:30:32 +0000

    feat: Create two more public constants called PRODUCTS_URL and PRICES_URL, and one private constant called BASE_URL

--- src/main/java/com/api/component/Constants.java
+++ src/main/java/com/api/component/Constants.java
@@ -3,6 +3,9 @@ package com.api.component;
 public final class Constants {
 
     public static final String TIMEZONE = "America/Sao_Paulo";
+    private final static String BASE_URL = "http://localhost/api/";
+    public static final String PRICES_URL = BASE_URL+"prices?barcode=";
+    public static final String PRODUCTS_URL = BASE_URL+"products";
 
     private Constants() {}
 }
