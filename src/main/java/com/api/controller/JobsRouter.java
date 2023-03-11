0b157a7d2a954248f380c321012ec647ed9f5983
Neemias Santos <neemiassgc@outlook.com>
2023-03-11 10:28:34 -0300

    chore: Create JobsRouter controller class to handle jobs request

---
+++ src/main/java/com/api/controller/JobsRouter.java
@@ -0,0 +1,14 @@
+package com.api.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JobsRouter {
+
+    @GetMapping("api/jobs/updatePrices")
+    public HttpStatus doJob() {
+
+    }
+}
