724b3e901da9c93604a35f91c718f1719268b3ef
Neemias Santos <neemiassgc@outlook.com>
2023-03-11 11:12:33 -0300

    chore: Add Job dependency and logic to check for X-AppEngine-Cron header to run a job whether it's true

--- src/main/java/com/api/controller/JobsRouter.java
+++ src/main/java/com/api/controller/JobsRouter.java
@@ -1,14 +1,33 @@
 package com.api.controller;
 
+import com.api.job.Job;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Objects;
+
 @RestController
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
 public class JobsRouter {
 
-    @GetMapping("api/jobs/updatePrices")
-    public HttpStatus doJob() {
+    private final Job job;
 
+    @GetMapping("api/jobs/updatePrices")
+    public ResponseEntity<HttpStatus> doJob(final @RequestHeader HttpHeaders httpHeaders) {
+        final List<String> appEngineCronHeader = httpHeaders.get("X-Appengine-Cron");
+        if (Objects.isNull(appEngineCronHeader)) return ResponseEntity.badRequest().body(HttpStatus.BAD_REQUEST);
+        for (String header : appEngineCronHeader) System.out.println(header);
+        if (appEngineCronHeader.contains("true")) {
+            job.execute();
+            return ResponseEntity.ok(HttpStatus.OK);
+        }
+        return ResponseEntity.badRequest().body(HttpStatus.BAD_REQUEST);
     }
 }
