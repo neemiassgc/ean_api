611e86e4ba75cadb7ba02e7a688a7ec288f8c489
Neemias Santos <neemiassgc@outlook.com>
2023-03-15 10:56:30 -0300

    chore: Declare imports and final dependencies CacheManager Bean will need

--- src/main/java/com/api/service/CacheManager.java
+++ src/main/java/com/api/service/CacheManager.java
@@ -1,7 +1,16 @@
 package com.api.service;
 
+import com.api.entity.Product;
 import org.springframework.stereotype.Service;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.stream.Collectors;
 
 @Service
 public class CacheManager {
+
+    private final Set<Product> source = new TreeSet<>(Comparator.comparing(Product::getDescription));
+    private final ConcurrentMap<String, List<UUID>> cache = new ConcurrentHashMap<>();
 }
