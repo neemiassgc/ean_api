776745dd6d170bdbf3768a5dad23e67dfdc8ce8a
Neemias Santos <neemiassgc@outlook.com>
2023-03-16 18:38:26 -0300

    test: Add a unit test named given_a_key_should_return_all_products_from_cache_in_the_order

--- src/test/java/com/api/service/ProductCacheManagerTest.java
+++ src/test/java/com/api/service/ProductCacheManagerTest.java
@@ -1,10 +1,12 @@
 package com.api.service;
 
+import com.api.Resources;
 import com.api.entity.Product;
+import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import java.util.Comparator;
-import java.util.UUID;
+import java.util.*;
 
 public final class ProductCacheManagerTest {
 
@@ -13,6 +15,27 @@ public final class ProductCacheManagerTest {
     @BeforeEach
     void setup() {
         productCacheManager = new CacheManager<>(Comparator.comparing(Product::getDescription), Product::getId);
+    }
+
+    @Test
+    void given_a_key_should_return_all_products_from_cache_in_the_order() {
+        final List<Product> eightProducts = getProductsByIndexes(0, 10, 4, 7, 9, 2, 3, 11);
+        final String key = "eight";
+        productCacheManager.put(key, eightProducts);
+
+        final Optional<List<Product>> actualProducts = productCacheManager.get(key);
+
+        assertThat(actualProducts).isNotNull();
+        assertThat(actualProducts).isPresent();
+        assertThat(actualProducts.get()).satisfies(products -> {
+            assertThat(products).hasSize(8);
+            assertThat(products).extracting(Product::getDescription)
+                .containsExactly(
+                    "ACHOC PO NESCAU 800G", "MAIONESE QUERO 210G TP", "BISC WAFER TODDY 132G CHOC",
+                    "CAFE UTAM 500G", "LIMP M.USO OMO 500ML DESINF HERBAL", "BALA GELATINA FINI 500G BURGUER",
+                    "BISC ROSQ MARILAN 350G INT", "MILHO VDE PREDILECTA 170G LT"
+                );
+        });
     }
 
     private List<Product> getProductsByIndexes(final int ...indexes) {
