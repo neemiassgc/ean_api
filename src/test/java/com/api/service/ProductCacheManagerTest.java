87f56e674f88157775198e744f986292e1e9fd2d
Neemias Santos <neemiassgc@outlook.com>
2023-03-16 16:02:55 -0300

    test: Setup ProductCacheManagerTest involving CacheManager instantiation

--- src/test/java/com/api/service/ProductCacheManagerTest.java
+++ src/test/java/com/api/service/ProductCacheManagerTest.java
@@ -1,4 +1,17 @@
 package com.api.service;
 
+import com.api.entity.Product;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.Comparator;
+import java.util.UUID;
+
 public final class ProductCacheManagerTest {
+
+    private CacheManager<Product, UUID> productCacheManager;
+
+    @BeforeEach
+    void setup() {
+        productCacheManager = new CacheManager<>(Comparator.comparing(Product::getDescription), Product::getId);
+    }
 }
