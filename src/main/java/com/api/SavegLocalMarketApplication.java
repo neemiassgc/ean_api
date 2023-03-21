5f6d76a8655b508ebbb7ce2dfd6f824fa5f43deb
Neemias Santos <neemiassgc@outlook.com>
2023-03-20 22:07:57 -0300

    chore: Add caching support with @EnableCaching annotation

--- src/main/java/com/api/SavegLocalMarketApplication.java
+++ src/main/java/com/api/SavegLocalMarketApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.hateoas.HypermediaAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = HypermediaAutoConfiguration.class)
+@EnableCaching
 public class SavegLocalMarketApplication {
 
 	public static void main(String[] args) {
