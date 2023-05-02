3fc7e03c3a07056b9d161b73506d12be10b02a2d
Neemias Santos <neemiassgc@outlook.com>
2023-05-02 18:43:18 -0300

    feat: Create a new package named component and add a new bean called Interceptor to it

---
+++ src/main/java/com/api/component/Interceptor.java
@@ -0,0 +1,9 @@
+package com.api.component;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class Interceptor implements HandlerInterceptor {
+    
+}
