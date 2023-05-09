b09509173b6776e3c847a6e88e28e9fa6f736f97
Neemias Santos <neemiassgc@outlook.com>
2023-05-09 12:02:24 -0300

    refactor: Add Lombok's @RequiredArgsConstructor annotation to CachingInterceptor

--- src/main/java/com/api/component/CachingInterceptor.java
+++ src/main/java/com/api/component/CachingInterceptor.java
@@ -3,6 +3,7 @@ package com.api.component;
 import com.api.entity.Product;
 import com.api.service.CacheManager;
 import com.api.utility.Constants;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.lang.NonNull;
@@ -18,10 +19,10 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Component
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
 public class CachingInterceptor implements HandlerInterceptor {
 
-    @Autowired
-    private CacheManager<Product, UUID> productCacheManager;
+    private final CacheManager<Product, UUID> productCacheManager;
 
     @Override
     public boolean preHandle(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull Object handler) {
