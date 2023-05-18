10ca07e51dee43b516b6fee4993416fc19d7a8ff
Neemias Santos <neemiassgc@outlook.com>
2023-05-18 11:31:42 -0300

    test: Create a new unit test named should_not_run_the_job_if_X_Appengine_Cron_header_is_missing

--- src/test/java/com/api/controller/JobsRouterTest.java
+++ src/test/java/com/api/controller/JobsRouterTest.java
@@ -1,11 +1,28 @@
 package com.api.controller;
 
+import com.api.entity.Product;
 import com.api.job.Job;
+import com.api.service.CacheManager;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = {JobsRouter.class, GlobalErrorHandlingController.class})
 @AutoConfigureMockMvc(printOnlyOnFailure = false)
@@ -16,6 +33,23 @@ public final class JobsRouterTest {
 
     @MockBean
     private Job job;
+
+    private final String URI = "/api/jobs/updatePrices";
+
+    @Test
+    @DisplayName("If X-Appegine-Cron is missing then api/jobs/updatePrices -> 400 BAD_REQUEST")
+    void should_not_run_the_job_if_X_Appengine_Cron_header_is_missing() throws Exception {
+        mockMvc.perform(get(URI)
+            .accept(MediaType.ALL)
+            .characterEncoding(StandardCharsets.UTF_8)
+        )
+        .andExpect(status().isBadRequest())
+        .andExpect(content().contentType("text/plain;charset=UTF-8"))
+        .andExpect(content().string("BAD_REQUEST"));
+
+        verify(job, never()).execute();
+    }
+
 
     @TestConfiguration
     static class TestConfig {
