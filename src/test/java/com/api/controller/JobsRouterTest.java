14a8ef4f9a537acc32b52d61cf5cab9d272c695b
Neemias Santos <neemiassgc@outlook.com>
2023-05-19 17:50:23 -0300

    Test: Extract redundant code into a private shared method named testWithHeader

--- src/test/java/com/api/controller/JobsRouterTest.java
+++ src/test/java/com/api/controller/JobsRouterTest.java
@@ -11,10 +11,15 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 import java.util.UUID;
 
 import static org.mockito.Mockito.*;
@@ -37,13 +42,7 @@ public final class JobsRouterTest {
     @Test
     @DisplayName("If X-Appegine-Cron is missing then api/jobs/updatePrices -> 400 BAD_REQUEST")
     void should_not_run_the_job_if_X_Appengine_Cron_header_is_missing() throws Exception {
-        mockMvc.perform(get(URI)
-            .accept(MediaType.ALL)
-            .characterEncoding(StandardCharsets.UTF_8)
-        )
-        .andExpect(status().isBadRequest())
-        .andExpect(content().contentType("text/plain;charset=UTF-8"))
-        .andExpect(content().string("BAD_REQUEST"));
+        testWithHeader(HttpStatus.BAD_REQUEST, "BAD_REQUEST", null);
 
         verify(job, never()).execute();
     }
@@ -51,14 +50,7 @@ public final class JobsRouterTest {
     @Test
     @DisplayName("If X-Appegine-Cron is not set to true then api/jobs/updatePrices -> 400 BAD_REQUEST")
     void should_not_run_the_job_if_X_Appengine_Cron_header_is_not_true() throws Exception {
-        mockMvc.perform(get(URI)
-            .accept(MediaType.ALL)
-            .header("X-Appengine-Cron", "false")
-            .characterEncoding(StandardCharsets.UTF_8)
-        )
-        .andExpect(status().isBadRequest())
-        .andExpect(content().contentType("text/plain;charset=UTF-8"))
-        .andExpect(content().string("BAD_REQUEST"));
+        testWithHeader(HttpStatus.BAD_REQUEST, "BAD_REQUEST", "false");
 
         verify(job, never()).execute();
     }
@@ -66,14 +58,7 @@ public final class JobsRouterTest {
     @Test
     @DisplayName("If X-Appegine-Cron is set to anything else than true then api/jobs/updatePrices -> 400 BAD_REQUEST\"")
     void should_not_run_the_job_if_X_Appengine_Cron_header_is_set_to_anything_else_than_true() throws Exception {
-        mockMvc.perform(get(URI)
-            .accept(MediaType.ALL)
-            .header("X-Appengine-Cron", "apple")
-            .characterEncoding(StandardCharsets.UTF_8)
-        )
-        .andExpect(status().isBadRequest())
-        .andExpect(content().contentType("text/plain;charset=UTF-8"))
-        .andExpect(content().string("BAD_REQUEST"));
+        testWithHeader(HttpStatus.BAD_REQUEST, "BAD_REQUEST", "apple");
 
         verify(job, never()).execute();
     }
@@ -83,17 +68,27 @@ public final class JobsRouterTest {
     void should_run_the_job_succesfully_when_X_Appengine_Cron_header_is_set_to_true() throws Exception {
         doNothing().when(job).execute();
 
-        mockMvc.perform(get(URI)
-            .accept(MediaType.ALL)
-            .header("X-Appengine-Cron", "true")
-            .characterEncoding(StandardCharsets.UTF_8)
-        )
-        .andExpect(status().isOk())
-        .andExpect(content().contentType("text/plain;charset=UTF-8"))
-        .andExpect(content().string("OK"));
+        testWithHeader(HttpStatus.OK, "OK", "true");
 
         verify(job, times(1)).execute();
         verifyNoMoreInteractions(job);
+    }
+
+    private void testWithHeader(
+        @NonNull final HttpStatus expectedHttpStatus,
+        @NonNull final String expectedContent,
+        @Nullable final String headerValue
+    ) throws Exception {
+        final MockHttpServletRequestBuilder requestBuilder =
+            get(URI).accept(MediaType.ALL).characterEncoding(StandardCharsets.UTF_8);
+
+        if (Objects.nonNull(headerValue))
+            requestBuilder.header("X-Appengine-Cron", headerValue);
+
+        mockMvc.perform(requestBuilder)
+            .andExpect(status().is(expectedHttpStatus.value()))
+            .andExpect(content().contentType("text/plain;charset=UTF-8"))
+            .andExpect(content().string(expectedContent));
     }
 
     @TestConfiguration
